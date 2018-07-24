8d99ef1d95baedd0915a71a370136616a84a9eff
adamstruck <strucka@ohsu.edu>
2018-07-24 13:06:56 -0700

    updates to ensembl gene id lookup util

--- src/bmeg/models/conversions.py
+++ src/bmeg/models/conversions.py
@@ -3,32 +3,34 @@ from bmeg.models.ensembl_lookup import hugo_ensembl
 import mygene
 
 
-def query_mygeneinfo_for_ensembl_gene(ids):
+def query_mygeneinfo_for_ensembl_gene(id):
     """
     Query http://mygene.info/v3/api for ensembl gene id
     """
+    if not isinstance(id, str):
+        raise TypeError("expected str")
+
     mg = mygene.MyGeneInfo()
     res = mg.querymany(
-        ids,
+        id,
         scopes="accession,alias,ensembl.gene,ensembl.transcript,ensembl.protein,\
         entrezgene,name,refseq,symbol,unigene,uniprot",
         fields="ensembl.gene",
         species="human"
     )
-    if 'notfound' in res[0]:
-        raise RuntimeError("query for %s returned no hits" % id)
-
-    # TODO
-    # figure out how to handle the case where query returns "ensembl" as a list
-    if isinstance(res[0]["ensembl"], list):
-        res[0]["ensembl"] = res[0]["ensembl"][0]
 
     # TODO
     # is it ok to always take the highest scoring hit?
-    if isinstance(ids, str):
-        return res[0]["ensembl"]["gene"]
-    else:
-        return [x["ensembl"]["gene"] for x in res]
+    top_hit = res[0]
+    if 'notfound' in top_hit or "ensembl" not in top_hit:
+        raise RuntimeError("query for %s returned no hits" % id)
+
+    # TODO
+    # should we always pick the first item if 'ensembl' is a list?
+    if isinstance(top_hit["ensembl"], list):
+        top_hit["ensembl"] = top_hit["ensembl"][0]
+
+    return top_hit["ensembl"]["gene"]
 
 
 def ensembl_gene_lookup(id):
