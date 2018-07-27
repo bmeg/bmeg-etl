cf76c0a2dac7b277407f20a1d884a6b362a8401f
Alex Buchanan <buchanae@ohsu.edu>
2018-07-27 10:36:53 -0700

    emitter: bson and msgpack experiments

--- src/bmeg/models/emitter.py
+++ src/bmeg/models/emitter.py
@@ -1,13 +1,44 @@
 import json
+import bson
+import msgpack
 import os
 
 from bmeg.models.vertex_models import Vertex
 from bmeg.models.edge_models import Edge
 
+class MsgpackEmitter:
+    def __init__(self, prefix, **kwargs):
+        self.handles = filehandler(prefix, "msgp", mode="wb")
+        self.emitter = emitter(**kwargs)
+
+    def close(self):
+        self.handles.close()
+
+    def emit(self, obj):
+        d = self.emitter.emit(obj)
+        fh = self.handles[obj]
+        b = msgpack.dumps(d)
+        fh.write(b)
+        #fh.write(os.linesep)
+
+class BSONEmitter:
+    def __init__(self, prefix, **kwargs):
+        self.handles = filehandler(prefix, "bson", mode="wb")
+        self.emitter = emitter(**kwargs)
+
+    def close(self):
+        self.handles.close()
+
+    def emit(self, obj):
+        d = self.emitter.emit(obj)
+        fh = self.handles[obj]
+        b = bson.dumps(d)
+        fh.write(b)
+        #fh.write(os.linesep)
 
 class JSONEmitter:
     def __init__(self, prefix, **kwargs):
-        self.handles = filehandler(prefix)
+        self.handles = filehandler(prefix, "json")
         self.emitter = emitter(**kwargs)
 
     def close(self):
@@ -50,14 +81,14 @@ class emitter:
             del data["to_gid"]
 
         # delete null values
-        if not self.preserve_nulls:
+        if not self.preserve_null:
             remove = [k for k in data if data[k] is None]
             for k in remove:
                 del data[k]
 
         if isinstance(obj, Vertex):
             dumped = {
-                "gid": self.gid,
+                "gid": obj.gid,
                 "label": label,
                 "data": data
             }
@@ -83,11 +114,13 @@ class filehandler:
 
     This is an internal helper.
     """
-    def __init__(self, prefix):
+    def __init__(self, prefix, extension, mode="w"):
         self.prefix = prefix
+        self.extension = extension
+        self.mode = mode
         self.handles = {}
 
-    def __get__(self, obj):
+    def __getitem__(self, obj):
         label = obj.__class__.__name__
 
         if isinstance(obj, Vertex):
@@ -97,12 +130,12 @@ class filehandler:
         else:
             suffix = "Unknown"
 
-        fname = "%s.%s.%s.json" % (self.prefix, label, suffix)
+        fname = "%s.%s.%s.%s" % (self.prefix, label, suffix, self.extension)
 
         if fname in self.handles:
             return self.handles[fname]
         else:
-            fh = open(fname, "w")
+            fh = open(fname, self.mode)
             self.handles[fname] = fh
             return fh
         
