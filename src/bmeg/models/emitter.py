dbc506a15c565f6ec65dc8626fd3e50f68cc9141
Alex Buchanan <buchanae@ohsu.edu>
2018-07-27 09:45:46 -0700

    emitter: refactor dump() responsibility

--- src/bmeg/models/emitter.py
+++ src/bmeg/models/emitter.py
@@ -1,17 +1,25 @@
+import json
+import os
+
 from bmeg.models.vertex_models import Vertex
 from bmeg.models.edge_models import Edge
 
 
 class Emitter:
-    def __init__(self, prefix):
+    def __init__(self, prefix, preserve_null=False):
         self.prefix = prefix
         self.handles = {}
+        self.preserve_null = preserve_null
 
     def close(self):
         for fname, fh in self.handles.items():
             fh.close()
 
     def emit(self, obj):
+
+        if not obj.gid:
+            raise ValueError("gid is empty")
+
         label = obj.__class__.__name__
 
         if isinstance(obj, Vertex):
@@ -29,6 +37,41 @@ class Emitter:
             fh = open(fname, "w")
             self.handles[fname] = fh
 
-        fh.write(obj.dump())
-        fh.write("\n")
+        data = dict(obj.__dict__)
+
+        if "gid" in data:
+            del data["gid"]
+
+        if "from_gid" in data:
+            del data["from_gid"]
+
+        if "to_gid" in data:
+            del data["to_gid"]
+
+        # delete null values
+        if not self.preserve_nulls:
+            remove = [k for k in data if data[k] is None]
+            for k in remove:
+                del data[k]
+
+        if isinstance(obj, Vertex):
+            dumped = json.dumps({
+                "gid": self.gid,
+                "label": label,
+                "data": data
+            })
+
+        elif isinstance(obj, Edge):
+            suffix = "Edge"
+            dumped = json.dumps({
+                "gid": obj.gid,
+                "label": label,
+                "from": obj.from_gid,
+                "to": obj.to_gid,
+                "data": data
+            })
+
+        fh.write(dumped)
+        fh.write(os.linesep)
+
         return
