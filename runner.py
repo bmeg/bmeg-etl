697e9e52dfccf147ca9fb9e9ad39ab848ab2806b
adamstruck <strucka@ohsu.edu>
2018-07-30 15:16:06 -0700

    fix style runner.py

--- runner.py
+++ runner.py
@@ -107,8 +107,8 @@ if __name__ == "__main__":
     parser_run.add_argument("--dry-run", action="store_true",
                             help="print generated CWL inputs and exit")
     parser_run.add_argument("--exec-dir", default=".",
-                            help="base directory to use for cwltool '--outdir',
-                            '--tmpdir-prefix', '--tmp-outdir-prefix',
+                            help="base directory to use for cwltool '--outdir', \
+                            '--tmpdir-prefix', '--tmp-outdir-prefix', \
                             '--cachedir'")
 
     args = parser.parse_args()
