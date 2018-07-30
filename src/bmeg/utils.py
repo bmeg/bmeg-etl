712aed08d490adf61d77ff87280504d4bda9c6e0
adamstruck <strucka@ohsu.edu>
2018-07-30 14:45:25 -0700

    added enfore_types decorator

--- src/bmeg/utils.py
+++ src/bmeg/utils.py
@@ -1,3 +1,52 @@
+import inspect
+import typing
+
+from contextlib import suppress
+from functools import wraps
+
+
+def enforce_types(callable):
+    spec = inspect.getfullargspec(callable)
+
+    def check_types(*args, **kwargs):
+        parameters = dict(zip(spec.args, args))
+        parameters.update(kwargs)
+        for name, value in parameters.items():
+            # Assume un-annotated parameters can be any type
+            with suppress(KeyError):
+                type_hint = spec.annotations[name]
+                if isinstance(type_hint, typing._SpecialForm):
+                    # No check for typing.Any, typing.Union, typing.ClassVar
+                    # (without parameters)
+                    continue
+                try:
+                    actual_type = type_hint.__origin__
+                except AttributeError:
+                    actual_type = type_hint
+                if isinstance(actual_type, typing._SpecialForm):
+                    # case of typing.Union[…] or typing.ClassVar[…]
+                    actual_type = type_hint.__args__
+
+                if not isinstance(value, actual_type):
+                    raise TypeError(
+                        "Unexpected type for '{}' (expected {} but found {})".
+                        format(name, type_hint, type(value))
+                    )
+
+    def decorate(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            check_types(*args, **kwargs)
+            return func(*args, **kwargs)
+        return wrapper
+
+    if inspect.isclass(callable):
+        callable.__init__ = decorate(callable.__init__)
+        return callable
+
+    return decorate(callable)
+
+
 def set_gid(obj, gid):
     object.__setattr__(obj, "gid", gid)
 
