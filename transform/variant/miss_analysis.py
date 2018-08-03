ce8687772b210a11863a9a185de6e86fe4070439
Brian Walsh <brian@bwalsh.com>
2018-08-03 11:17:11 -0700

    reformat for flake8

--- transform/variant/miss_analysis.py
+++ transform/variant/miss_analysis.py
@@ -3,21 +3,7 @@
 """ summarize logs to analyze misses  """
 
 # print total imported
-# cat  maf_transform.log | sed 's/2018.* - .* - //' | grep imported  | tail -1 | sed 's/imported //'
-
-# cat  maf_transform.log | sed 's/2018.* - .* - //' | grep stage | python miss_analysis.py  $(cat  maf_transform.log | sed 's/2018.* - .* - //' | grep imported  | tail -1 | sed 's/imported //') |  jq .
-# {
-#   "myvariantinfo_nofind": 26302,
-#   "myvariantinfo_exception": 16,
-#   "dbSNP_mismatch": 2926,
-#   "Variant_Type": {
-#     "ONP": 13,
-#     "DEL": 4582,
-#     "TNP": 4,
-#     "SNP": 21782,
-#     "INS": 2863
-#   }
-# }
+# see README.md for example how to call
 from __future__ import print_function
 
 import json
@@ -52,11 +38,11 @@ for line in sys.stdin:
         ref = line['hit']['vcf']['ref']
         alt = line['hit']['vcf']['alt']
         if len(ref) > 1:
-            ref_diff = sum ([ {' ':0, '-':1,  '+':1}[d[0]]  for d in difflib.ndiff(line['reference_bases'],ref)])
+            ref_diff = sum([{' ': 0, '-': 1,  '+': 1}[d[0]] for d in difflib.ndiff(line['reference_bases'], ref)])  # noqa
             if ref_diff == 1:
                 summary['ref_off_by_one'] += 1
         if len(alt) > 1:
-            alt_diff = sum ([ {' ':0, '-':1,  '+':1}[d[0]]  for d in difflib.ndiff(line['alternate_bases'],alt)])
+            alt_diff = sum([{' ': 0, '-': 1, '+': 1}[d[0]] for d in difflib.ndiff(line['alternate_bases'], alt)])  # noqa
             if alt_diff == 1:
                 summary['alt_off_by_one'] += 1
 
@@ -65,11 +51,11 @@ summary['missing_snp'] = summary['dbSNP_RS'].get('novel', 0) \
 
 report = 'misses = {}%; novel = {}%; wildcard_misses ={}%; dbSNP_mismatch ={}%'
 summary['report'] = report.format(
-    ((summary['myvariantinfo_nofind']+summary['dbSNP_mismatch']) *100)/summary['total'],
+    ((summary['myvariantinfo_nofind']+summary['dbSNP_mismatch']) * 100)/summary['total'],  # noqa
     (summary['missing_snp']*100)/summary['total'],
-    ((summary['alternate_wildcard'] + summary['reference_wildcard'])*100)/summary['total'],
+    ((summary['alternate_wildcard'] + summary['reference_wildcard']) * 100)/summary['total'],  # noqa
     (summary['dbSNP_mismatch']*100)/summary['total'],
-)  # noqa
+)
 
 del summary['Feature']
 del summary['dbSNP_RS']
