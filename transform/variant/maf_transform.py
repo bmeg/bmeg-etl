0b27810ff75ad4201ae63848b4a568ef650c7f2f
Brian Walsh <brian@bwalsh.com>
2018-08-06 13:45:10 -0700

    re-introduce class

--- transform/variant/maf_transform.py
+++ transform/variant/maf_transform.py
@@ -7,8 +7,8 @@ import csv
 import gzip
 import sys
 
-from bmeg.vertex import Biosample, Callset, Gene
-from bmeg.edge import AlleleCall, CallsetFor, AlleleIn
+from bmeg.vertex import Biosample
+from bmeg.edge import CallsetFor, AlleleIn
 from bmeg.emitter import JSONEmitter as Emitter
 from bmeg.util.cli import default_argument_parser
 from bmeg.util.logging import default_logging
@@ -22,18 +22,18 @@ from itertools import islice
 
 # center = 2
 # ncbi_build = 3
-chromosome = "Chromosome"  # 4
-start = ["Start_Position", "Start_position"]  # 5
-end = ["End_Position", "End_position"]  # 6
+CHROMOSOME = "Chromosome"  # 4
+START = ["Start_Position", "Start_position"]  # 5
+END = ["End_Position", "End_position"]  # 6
 # strand = 7
-variant_type = "Variant_Type"  # 9
-reference_allele = "Reference_Allele"  # 10
-# tumor_allele1 = "Tumor_Seq_Allele1"  # 11
+VARIANT_TYPE = "Variant_Type"  # 9
+REFERENCE_ALLELE = "Reference_Allele"  # 10
+TUMOR_ALLELE = "Tumor_Seq_Allele1"  # 11
 tumor_allele2 = "Tumor_Seq_Allele2"  # 12
 # annotation_transcript = "Annotation_Transcript"  # 14
 
-tumor_sample_barcode = "Tumor_Sample_Barcode"  # 15
-normal_sample_barcode = "Matched_Norm_Sample_Barcode"  # 16
+TUMOR_SAMPLE_BARCODE = "Tumor_Sample_Barcode"  # 15
+NORMAL_SAMPLE_BARCODE = "Matched_Norm_Sample_Barcode"  # 16
 
 # normal_allele1 = 17
 # normal_allele2 = 18
@@ -51,8 +51,8 @@ normal_sample_barcode = "Matched_Norm_Sample_Barcode"  # 16
 # entrez_gene_id = 1
 # variant_classification = 8
 
-feature = "Feature"  # 48
-feature_type = "Feature_type"  # 49
+FEATURE = "Feature"  # 48
+FEATURE_TYPE = "Feature_type"  # 49
 dbSNP_RS = "dbSNP_RS"  # 13
 
 
@@ -67,181 +67,156 @@ def get_value(d, keys, default):
     return default
 
 
-def _read_maf(mafpath, gz, skip=0, harvest=True):
-    """ generator for each line in maf """
-    if gz or 'gz' in mafpath:
-        inhandle = gzip.open(mafpath, mode='rt')
-    else:
-        inhandle = open(mafpath)
-    reader = csv.DictReader(inhandle, delimiter="\t")
-    logging.info('skipping: {}'.format(skip))
-    reader = islice(reader, skip, None)
-    for line in reader:
-        yield line
-    inhandle.close()
+class MAFTransformer():
+    def read_maf(self, mafpath, gz, skip=0, harvest=True):
+        """ generator for each line in maf """
+        if gz or 'gz' in mafpath:
+            inhandle = gzip.open(mafpath, mode='rt')
+        else:
+            inhandle = open(mafpath)
+        reader = csv.DictReader(inhandle, delimiter="\t")
+        logging.info('skipping: {}'.format(skip))
+        reader = islice(reader, skip, None)
+        for line in reader:
+            yield line
+        inhandle.close()
 
+    def allele_call_maker(self, allele, line=None):
+        """ override, create call from line """
+        pass
 
-def _allele_call_maker(allele, line=None):
-    """ create call from line """
-    keys = ['t_depth', 't_ref_count', 't_alt_count', 'n_depth',
-            'n_ref_count', 'n_alt_count', 'FILTER',
-            'Match_Norm_Seq_Allele1',	'Match_Norm_Seq_Allele2',
-            'Tumor_Seq_Allele1', 'Tumor_Seq_Allele2',
-            ]
-    info = {}
-    for k in keys:
-        info[k] = get_value(line, k, None)
-    return AlleleCall(info)
+    def barcode_to_sampleid(self, barcode):  # pragma nocover
+        """ override, decode barcode """
+        return barcode
 
+    def callset_maker(self, allele, source, centerCol, method, line):  # noqa pragma nocover
+        """ override, create callset from line """
+        print('override me')
+        pass
 
-def _tcga_aliquot2sample_barcode(barcode):
-    """ create tcga sample barcode """
-    return "-".join(barcode.split("-")[0:4])
+    def create_gene_gid(self, line):  # pragma nocover
+        """ override, create gene_gid from line """
+        pass
 
+    def create_allele_dict(self, line, genome='GRCh37'):
+        ''' return properly named allele dictionary, populated from line'''
+        # collect CURIES that apply to allele
+        annotations = []
+        annotations.append('{}:{}'.format(
+            VARIANT_TYPE, line.get(VARIANT_TYPE, None)
+            ))
+        annotations.append('{}:{}'.format(
+            FEATURE_TYPE, line.get(FEATURE_TYPE, None)
+            ))
+        annotations.append('{}:{}'.format(
+            FEATURE, line.get(FEATURE, None)
+            ))
+        annotations.append('{}:{}'.format(
+            dbSNP_RS, line.get(dbSNP_RS, None)
+            ))
 
-def _callset_maker(allele, source, centerCol, method, line):
-    """ create callset from line """
-    barcode = line[tumor_sample_barcode]
-    sample = barcode
-    if source == 'tcga':
-        sample = _tcga_aliquot2sample_barcode(barcode)
-    sample = Biosample.make_gid(sample)
-    sample_callsets = []
-    sample_calls = []
-    if centerCol in line:
-        for c in line[centerCol].split("|"):
-            center = c.replace("*", "")
-            # callset_id = "%s:%s" % (sample, center)
-            callset = Callset(sample,
-                              Biosample.make_gid(line[normal_sample_barcode]),
-                              center, source)
-            sample_callsets.append(callset)
-            sample_calls.append((_allele_call_maker(allele, line),
-                                 callset.gid()))
-    else:
-        callset = Callset(sample,
-                          Biosample.make_gid(line[normal_sample_barcode]),
-                          method, source)
-        sample_callsets.append(callset)
-        sample_calls.append((_allele_call_maker(allele, line),
-                             callset.gid()))
-    return sample_calls, sample_callsets
+        return {
+            'genome': genome,
+            'chromosome': line[CHROMOSOME],
+            'start': int(get_value(line, START, None)),
+            'end': int(get_value(line, END, None)),
+            'reference_bases': line[REFERENCE_ALLELE],
+            'alternate_bases': line[TUMOR_ALLELE],
+            # ,myvariantinfo: dict
+            'annotations': annotations,
+        }
 
-
-def _allele_dict(line, genome='GRCh37'):
-    ''' return properly named allele dictionary, populated form line'''
-    # collect CURIES that apply to allele
-    annotations = []
-    annotations.append('{}:{}'.format(
-        variant_type, line[variant_type]))
-    annotations.append('{}:{}'.format(
-        feature_type, line[feature_type]))
-    annotations.append('{}:{}'.format(
-        feature, line[feature]))
-    annotations.append('{}:{}'.format(
-        dbSNP_RS, line[dbSNP_RS]))
-
-    return {
-        'genome': genome,
-        'chromosome': line[chromosome],
-        'start': int(get_value(line, start, None)),
-        'end': int(get_value(line, end, None)),
-        'reference_bases': line[reference_allele],
-        'alternate_bases': line[tumor_allele2],
-        # ,myvariantinfo: dict
-        'annotations': annotations,
-    }
-
-
-def _allele_maker(line, harvest, filter):
-    """ worker task to create and/or harvest allele from line """
-    allele_dict = _allele_dict(line)
-    return allele_harvester.harvest(**allele_dict,
-                                    harvest=harvest,
-                                    filter=filter), line
-    # if harvest:
-    #     return allele_harvester.harvest(**allele_dict), line
-    # return allele_harvester.create(**allele_dict), line
-
-
-def _multithreading(func, lines, max_workers, harvest, filter):
-    """
-    Create a thread pool and create alleles
-    """
-    # limit queue size == number of workers
-    # see https://stackoverflow.com/questions/48263704/threadpoolexecutor-how-to-limit-the-queue-maxsize  # noqa
-
-    for chunk in chunked(lines, max_workers):
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            futures = (executor.submit(func, line, harvest, filter) for line in chunk)  # noqa
-            for future in as_completed(futures):
-                yield future.result()
-
-
-def maf_convert(emitter, mafpath, workers, source='tcga', genome='GRCh37',
-                method='variant', gz=False, centerCol='Center', skip=0,
-                harvest=True, filter=[]):
-    """
-    emitter -  a way to write output
-    mafpath - a file to read
-    source - special handling if 'tcga'
-    genome - reference_genome e.g. GRCh37
-    method - call's method
-    gz - is mafpath a gz
-    skip - ignore first N lines
-    """
-
-    logging.info('converting maf: ' + mafpath)
-    my_callsets_ids = set()
-    c = skip
-    for allele, line in _multithreading(_allele_maker,
-                                        _read_maf(mafpath, gz, skip),
-                                        max_workers=workers,
+    def allele_maker(self, line, harvest, filter):
+        """ worker task to create and/or harvest allele from line """
+        allele_dict = self.create_allele_dict(line)
+        return allele_harvester.harvest(**allele_dict,
                                         harvest=harvest,
-                                        filter=filter):
-        # if allele was filtered out
-        if not allele:
-            continue
-        # save the allele that was created
-        emitter.emit_vertex(allele)
-        # create edge between the allele and the callset
-        call_tuples, callsets = _callset_maker(allele, source, centerCol,
-                                               method, line)
-        # save the calls
-        for call_tuple in call_tuples:
-            call = call_tuple[0]
-            callset_gid = call_tuple[1]
-            emitter.emit_edge(call, allele.gid(), callset_gid)
-        # many callsets can be created, emit only uniques
-        for callset in callsets:
-            if callset.gid not in my_callsets_ids:
-                my_callsets_ids.add(callset.gid)
-                emitter.emit_vertex(callset)
-                emitter.emit_edge(CallsetFor(), callset.gid(),
-                                  callset.normal_biosample_id)
-                emitter.emit_edge(CallsetFor(), callset.gid(),
-                                  callset.tumor_biosample_id)
+                                        filter=filter), line
 
-        # create edge to gene
-        ensembl_id = line.get('Gene', None)
-        gene_gid = Gene.make_gid(gene_id=ensembl_id)
-        emitter.emit_edge(AlleleIn(), allele.gid(), gene_gid)
-        # log progress
-        c += 1
-        if c % 1000 == 0:  # pragma nocover
-            logging.info('imported {}'.format(c))
-    logging.info('imported {}'.format(c))
+    def multithreading(self, func, lines, max_workers, harvest, filter):
+        """
+        Create a thread pool and create alleles
+        """
+        # limit queue size == number of workers
+        # see https://stackoverflow.com/questions/48263704/threadpoolexecutor-how-to-limit-the-queue-maxsize  # noqa
+
+        for chunk in chunked(lines, max_workers):
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                futures = (executor.submit(func, line, harvest, filter) for line in chunk)  # noqa
+                for future in as_completed(futures):
+                    yield future.result()
+
+    def maf_convert(self, emitter, mafpath, workers, source='tcga',
+                    genome='GRCh37',
+                    method='variant', gz=False, centerCol='Center', skip=0,
+                    harvest=True, filter=[]):
+        """
+        emitter -  a way to write output
+        mafpath - a file to read
+        source - special handling if 'tcga'
+        genome - reference_genome e.g. GRCh37
+        method - call's method
+        gz - is mafpath a gz
+        skip - ignore first N lines
+        """
+
+        logging.info('converting maf: ' + mafpath)
+        my_callsets_ids = set()
+        c = skip
+        for allele, line in self.multithreading(self.allele_maker,
+                                                self.read_maf(
+                                                    mafpath, gz, skip),
+                                                max_workers=workers,
+                                                harvest=harvest,
+                                                filter=filter):
+            # if allele was filtered out
+            if not allele:
+                continue
+            # save the allele that was created
+            emitter.emit_vertex(allele)
+            # create edge between the allele and the callset
+            call_tuples, callsets = self.callset_maker(allele, source,
+                                                       centerCol,
+                                                       method, line)
+            # save the calls
+            for call_tuple in call_tuples:
+                call = call_tuple[0]
+                callset_gid = call_tuple[1]
+                emitter.emit_edge(call, allele.gid(), callset_gid)
+            # many callsets can be created, emit only uniques
+            for callset in callsets:
+                if callset.gid not in my_callsets_ids:
+                    my_callsets_ids.add(callset.gid)
+                    emitter.emit_vertex(callset)
+                    if callset.normal_biosample_id:
+                        emitter.emit_edge(CallsetFor(), callset.gid(),
+                                          Biosample.make_gid(
+                                            callset.normal_biosample_id))
+                    emitter.emit_edge(CallsetFor(), callset.gid(),
+                                      Biosample.make_gid(
+                                        callset.tumor_biosample_id))
+
+            # create edge to gene
+            gene_gid = self.create_gene_gid(line)
+            if gene_gid:
+                emitter.emit_edge(AlleleIn(), allele.gid(), gene_gid)
+            # log progress
+            c += 1
+            if c % 1000 == 0:  # pragma nocover
+                logging.info('imported {}'.format(c))
+        logging.info('imported {}'.format(c))
 
 
-def convert(mafpath, prefix, workers=5, skip=0, harvest=True, filter=[]):
+def transform(mafpath, prefix, workers=5, skip=0, harvest=True, filter=[],
+              transformer=MAFTransformer()):
     """ entry point """
     emitter = Emitter(prefix=prefix)
-    maf_convert(emitter=emitter, mafpath=mafpath, workers=workers, skip=skip,
-                harvest=harvest, filter=filter)
+    transformer.maf_convert(emitter=emitter, mafpath=mafpath, workers=workers,
+                            skip=skip, harvest=harvest, filter=filter)
     emitter.close()
 
 
-if __name__ == '__main__':  # pragma: no cover
+def main(transformer=MAFTransformer()):  # pragma: no cover
     parser = default_argument_parser()
     parser.add_argument('--maf_file', type=str,
                         help='Path to the maf you want to import')
@@ -276,6 +251,10 @@ if __name__ == '__main__':  # pragma: no cover
                 line = line.strip()
                 filter[line] = None
 
-    convert(mafpath=options.maf_file, prefix=options.prefix,
-            workers=options.workers, skip=options.skip,
-            harvest=options.harvest, filter=filter)
+    transform(mafpath=options.maf_file, prefix=options.prefix,
+              workers=options.workers, skip=options.skip,
+              harvest=options.harvest, filter=filter, transformer=transformer)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
---
+++ transform/variant/tcga_maf_transform.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" transform a maf file into vertexs[variant, allele]   """
+from maf_transform import main, get_value, MAFTransformer
+from maf_transform import transform as parent_transform
+
+from bmeg.vertex import Biosample, Callset, Gene
+from bmeg.edge import AlleleCall
+
+TUMOR_SAMPLE_BARCODE = "Tumor_Sample_Barcode"  # 15
+NORMAL_SAMPLE_BARCODE = "Matched_Norm_Sample_Barcode"  # 16
+
+
+class TCGA_MAFTransformer(MAFTransformer):
+
+    def barcode_to_sampleid(self, barcode):
+        """ create tcga sample barcode """
+        return "-".join(barcode.split("-")[0:4])
+
+    def create_gene_gid(self, line):  # pragma nocover
+        """ override, create gene_gid from line """
+        ensembl_id = line.get('Gene', None)
+        return Gene.make_gid(gene_id=ensembl_id)
+
+    def allele_call_maker(self, allele, line=None):
+        """ create call from line """
+        keys = ['t_depth', 't_ref_count', 't_alt_count', 'n_depth',
+                'n_ref_count', 'n_alt_count', 'FILTER',
+                'Match_Norm_Seq_Allele1',	'Match_Norm_Seq_Allele2',
+                'Tumor_Seq_Allele1', 'Tumor_Seq_Allele2',
+                ]
+        info = {}
+        for k in keys:
+            info[k] = get_value(line, k, None)
+        return AlleleCall(info)
+
+    def callset_maker(self, allele, source, centerCol, method, line):
+        """ create callset from line """
+        barcode = line[TUMOR_SAMPLE_BARCODE]
+        sample = barcode
+        sample = self.barcode_to_sampleid(barcode)
+        sample = Biosample.make_gid(sample)
+        sample_callsets = []
+        sample_calls = []
+        if centerCol in line:
+            for c in line[centerCol].split("|"):
+                center = c.replace("*", "")
+                # callset_id = "%s:%s" % (sample, center)
+                callset = Callset(sample,
+                                  line[NORMAL_SAMPLE_BARCODE],
+                                  center, source)
+                sample_callsets.append(callset)
+                sample_calls.append((self.allele_call_maker(allele, line),
+                                     callset.gid()))
+        else:
+            callset = Callset(sample,
+                              line[NORMAL_SAMPLE_BARCODE],
+                              method, source)
+            sample_callsets.append(callset)
+            sample_calls.append((self.allele_call_maker(allele, line),
+                                 callset.gid()))
+        return sample_calls, sample_callsets
+
+
+def transform(mafpath, prefix, workers=5, skip=0, harvest=True, filter=[]):
+    return parent_transform(mafpath, prefix, workers, skip, harvest, filter,
+                            transformer=TCGA_MAFTransformer())
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
