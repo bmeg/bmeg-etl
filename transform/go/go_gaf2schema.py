bcb1f2b62a331d383cdeaec8530d54d568f214fb
Kyle Ellrott <kellrott@gmail.com>
2018-08-09 16:44:07 -0700

    Removing unused code

--- transform/go/go_gaf2schema.py
+++ transform/go/go_gaf2schema.py
@@ -3,8 +3,6 @@
 # parses GO annotation file
 # http://www.geneontology.org/doc/GO.references
 
-# curl -L -O http://geneontology.org/gene-associations/goa_human.gaf.gz
-# curl -O ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz
 
 import sys
 import json
@@ -14,23 +12,6 @@ from bmeg.vertex import Gene, GeneOntologyTerm
 from bmeg.edge import GeneOntologyAnnotation
 from bmeg.emitter import JSONEmitter
 
-def uniprot_ns(n):
-	return "uniprot:" + n
-
-def ensembl_ns(n):
-	return "ensembl:" + n
-
-def go_ns(n):
-	return "go:" + n
-
-def goref_ns(n):
-	return "goref:" + n
-
-def gene_ns(n):
-	return "gene:" + n
-
-def pubmed_ns(n):
-	return "pubmed:" + n
 
 UNIPROT_COL = 1
 SYMBOL_COL = 2
@@ -62,9 +43,7 @@ if __name__ == "__main__":
 				row = line.rstrip().rsplit("\t")
 				if row[UNIPROT_COL] in uniprot_2_ensembl:
 					go_id = row[GOID_COL]
-					#for r in row[SYMBOL_COL].split("|"):
-					#	if re.search(r'^[A-Z0-9]+$', r):
-					#		goa.genes.append( gene_ns(r) )
+
 					ensembl_id = uniprot_2_ensembl[row[UNIPROT_COL]]
 					evidence = row[EVIDENCE_COL]
 					references = []
