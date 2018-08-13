ef8597c84ddf0ed16e0f53fee7c8a250645fb86c
Alex Buchanan <buchanae@ohsu.edu>
2018-08-13 16:04:11 -0700

    transform/gtex: cleanup

--- transform/gtex/samples.py
+++ transform/gtex/samples.py
@@ -1,69 +1,58 @@
-from bmeg.util.cli import default_argument_parser
 from bmeg.emitter import JSONEmitter
 from bmeg.ioutils import read_tsv
 from bmeg.vertex import Aliquot, Biosample, Project, Individual
 from bmeg.edge import AliquotFor, BiosampleFor, InProject
 
 
-parser = default_argument_parser()
-parser.add_argument("samples_file")
-parser.add_argument("individuals_file")
-
-
 def extract_individual_id(sample_id):
     return "-".join(sample_id.split("-")[:2])
 
 
-def transform(emitter, samples, individuals):
-    project = Project("gtex", gdc_attributes={})
-    emitter.emit_vertex(project)
+emitter = JSONEmitter("gtex")
+samples = read_tsv("source/gtex/GTEx_v7_Annotations_SampleAttributesDS.txt")
+individuals = read_tsv("source/gtex/GTEx_v7_Annotations_SubjectPhenotypesDS.txt")
 
-    for row in individuals:
-        individual_id = row["SUBJID"]
-        i = Individual(
-            individual_id,
-            gdc_attributes={},
-            gtex_attributes=row,
-        )
-        emitter.emit_vertex(i)
-        emitter.emit_edge(
-            InProject(),
-            i.gid(),
-            project.gid(),
-        )
+project = Project("gtex", gdc_attributes={})
+emitter.emit_vertex(project)
 
-    for row in samples:
-        sample_id = row["SAMPID"]
-        individual_id = extract_individual_id(sample_id)
-        b = Biosample(
-            sample_id,
-            gdc_attributes={},
-            gtex_attributes=row,
-        )
-        emitter.emit_vertex(b)
-        emitter.emit_edge(
-            BiosampleFor(),
-            b.gid(),
-            Individual.make_gid(individual_id),
-        )
+for row in individuals:
+    individual_id = row["SUBJID"]
+    i = Individual(
+        individual_id,
+        gdc_attributes={},
+        gtex_attributes=row,
+    )
+    emitter.emit_vertex(i)
+    emitter.emit_edge(
+        InProject(),
+        i.gid(),
+        project.gid(),
+    )
 
-        a = Aliquot(
-            sample_id,
-            gdc_attributes={},
-        )
-        emitter.emit_vertex(a)
-        emitter.emit_edge(
-            AliquotFor(),
-            a.gid(),
-            b.gid(),
-        )
+for row in samples:
+    sample_id = row["SAMPID"]
+    individual_id = extract_individual_id(sample_id)
+    b = Biosample(
+        sample_id,
+        gdc_attributes={},
+        gtex_attributes=row,
+    )
+    emitter.emit_vertex(b)
+    emitter.emit_edge(
+        BiosampleFor(),
+        b.gid(),
+        Individual.make_gid(individual_id),
+    )
 
+    a = Aliquot(
+        sample_id,
+        gdc_attributes={},
+    )
+    emitter.emit_vertex(a)
+    emitter.emit_edge(
+        AliquotFor(),
+        a.gid(),
+        b.gid(),
+    )
 
-if __name__ == "__main__":
-    e = JSONEmitter("gtex")
-    s = read_tsv("source/gtex/GTEx_v7_Annotations_SampleAttributesDS.txt")
-    i = read_tsv("source/gtex/GTEx_v7_Annotations_SubjectPhenotypesDS.txt")
-
-    transform(e, s, i)
-
-    e.close()
+emitter.close()
