673dba8471cd615ec4e8b4f59b5a8b9dc90fe3a2
Alex Buchanan <buchanae@ohsu.edu>
2018-08-13 15:33:17 -0700

    transform/gdsc: separate ccle samples and gdsc drug response

--- src/bmeg/util/logging.py
+++ src/bmeg/util/logging.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ utility, minimal logging   """
-import logging
+from logging import *
 
 
 def default_logging(loglevel):  # pragma: no cover
     """ bear bones logging"""
     fmt = '%(asctime)s - %(levelname)s - %(threadName)s - %(message)s'
-    logging.basicConfig(level=loglevel, format=fmt)
+    basicConfig(level=loglevel, format=fmt)
+
+
+def simple_logging():
+    fmt = '%(levelname)s %(message)s'
+    basicConfig(level=INFO, format=fmt)
