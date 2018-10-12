354aac67799ee2193e84998992bc6a510e445a88
Brian Walsh <brian@bwalsh.com>
2018-10-12 01:28:15 +0000

    query examples

--- postgres/scripts/queries.sql
+++ postgres/scripts/queries.sql
@@ -435,3 +435,211 @@ FROM pg_tables t
 WHERE t.schemaname='public'
 ORDER BY 1,2;
 ```
+
+
+Allele:ab5f4bf29886c718f4b5ed285c7bbf838a0e91d4
+
+
+select
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+;
+
+
+
+select
+  count(distinct al.from) as "allele",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+      join edge al on (al.label = 'AlleleCall' and a.from = al.to)
+;
+
+
+
+select
+  count(distinct al.from) as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to and a.label = 'AliquotFor')
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to and c.label = 'CallsetFor')
+;
+
+
+
+
+explain select
+  count(distinct al.from) as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to)
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to)
+;
+
+
+
+explain select
+  count(distinct al.from) as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to)
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to)
+  WHERE
+    al.from = 'Allele:ab5f4bf29886c718f4b5ed285c7bbf838a0e91d4'
+;
+
+select
+  count(distinct al.from) as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  p.to as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to)
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to)
+  WHERE
+    al.from = 'Allele:ab5f4bf29886c718f4b5ed285c7bbf838a0e91d4'
+  GROUP BY
+    project
+;
+
+
+select
+  count(distinct al.from) as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  p.to as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to)
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to)
+  GROUP BY
+    project
+;
+
+
+
+Show the most 'popular' alleles
+
+```
+
+select
+  al.from  as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project"
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to)
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to)
+  GROUP BY
+    allele
+  HAVING
+   count(distinct p.to)  > 10
+  ORDER  BY project DESC;
+
+                     allele                      | callset | aliquot | biosample | individual | project
+-------------------------------------------------+---------+---------+-----------+------------+---------
+ Allele:ebc8cd9acf2198d45dbdfd764a69368dca489a0b |     296 |     296 |       296 |        164 |      37
+ Allele:8e9f5f0e7766550b409878711e929445645ba8c8 |     354 |     354 |       354 |        194 |      36
+ Allele:d54fe7c2a96ce7ac0f0f75096926c1436d61e5ad |     314 |     314 |       314 |        184 |      35
+ Allele:7d9fbcd1c09166fab2ea01d1bdd571255ddd830c |     210 |     210 |       210 |        116 |      34
+ Allele:2f29241c6df6db2090639f153379e3da7e1cf8e9 |     569 |     569 |       569 |        292 |      33
+ Allele:74db52a85f0d48bf3b1e6e6ebb10c68d9e929ca0 |     257 |     257 |       257 |        148 |      32
+ Allele:2d5c0debfaf3ba7a5e5e0614203512ff0a3a52d5 |     176 |     176 |       176 |         94 |      32
+ Allele:dd9394f9294c03b23da21f612bb88f22f40b6f87 |     469 |     469 |       469 |        263 |      30
+ Allele:ce45202f2eb047f1b22c13fb17c5f84cbb06e195 |     531 |     531 |       531 |        273 |      29
+ Allele:18f46c782a347c0ed6f7c454fcd7ad9c5e209965 |     160 |     160 |       160 |         89 |      29
+ Allele:e0cd8bc1a8be06e6a8d44c5fda6748de27b7b666 |     107 |     107 |       107 |         62 |      26
+ Allele:9a7be53d7094bef75f035c264a1f4e25896b440e |     173 |     173 |       173 |        104 |      25
+ Allele:4fc7d3a2bde4e19cd8c6260adc7ec8a252898eef |     395 |     395 |       395 |        213 |      24
+ Allele:26b1a6cb66bc58350ca21ef592b26d075a0a7a0f |     243 |     243 |       243 |        131 |      23
+ Allele:b52ced1e915b178dd364afac3458a17f6e65630c |     358 |     358 |       358 |        183 |      23
+ Allele:7fd5a7a99a15dd9695f369b656d15f008f9dad58 |      79 |      79 |        79 |         71 |      23
+ Allele:1fd3ee6ba5430e1702483055b912a6bda6db87c8 |     187 |     187 |       187 |         96 |      23
+ Allele:6794c0e81a961346669f667e696e566c7767059f |    1248 |    1248 |      1248 |        660 |      23
+ Allele:5d7a7f15245108d1c93e06df4442a8b6eac209ec |     254 |     254 |       254 |        158 |      22
+ Allele:8bdb32c48766f14bd6055150be4b6b1c2faf401a |      75 |      75 |        75 |         41 |      22
+ Allele:17a1c142dbae32abb5c5b11893aaeb31cd602c3d |     244 |     244 |       244 |        125 |      21
+ Allele:64882d92d160f0c43ad9cd427745fa83849777c6 |      88 |      88 |        88 |         47 |      21
+ Allele:494394b3839557c97fd380f96e94b39c23e4072e |      56 |      56 |        56 |         56 |      21
+ Allele:c87423aed4064e419cee7eb1d18564474dfadb34 |      53 |      53 |        53 |         51 |      21
+ Allele:b4aa2bfe2b2d6fd36d7756995c9024ce0f7881ee |      49 |      49 |        49 |         49 |      21
+
+ ....
+
+ Time: 116398.436 ms (01:56.398)
+ ```
+
+
+ Alleles with g2p entries
+ ```
+ select
+   al.to  as "allele",
+   count(distinct al.from) as "g2p_association"
+   from edge as al
+ WHERE
+   al.label = 'HasAlleleFeature'
+ GROUP BY
+   allele
+ HAVING
+   count(distinct al.from)  > 10
+ ORDER BY
+   "g2p_association" DESC
+ ;
+```
+
+
+
+
+
+select
+  count(distinct al.from)  as "allele",
+  count(distinct c.to) as "callset",
+  count(distinct a.from) as "aliquot",
+  count(distinct a.to) as "biosample",
+  count(distinct i.to) as "individual",
+  count(distinct p.to) as "project",
+  count(distinct ass.from) as "g2p_association",  
+  from edge as a
+    join edge i on (a.label = 'AliquotFor' and i.from = a.to and i.label = 'BiosampleFor')
+      join edge p on (p.label = 'InProject' and p.from = i.to)
+        join edge c on (c.label = 'CallsetFor' and a.from = c.to)
+          join edge al on (al.label = 'AlleleCall' and c.from = al.to)
+            join edge ass on (ass.label = 'HasAlleleFeature' and ass.to = al.from)
