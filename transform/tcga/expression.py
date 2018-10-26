fc26c874be8983bf9068b7c339fdc4aae104af8b
Brian Walsh <brian@bwalsh.com>
2018-10-26 19:48:17 +0000

    create N expression files; introduce test

--- transform/tcga/expression.py
+++ transform/tcga/expression.py
@@ -2,21 +2,33 @@ from collections import defaultdict
 import csv
 from glob import glob
 import gzip
+import os.path
+import logging
+import subprocess
+import sys
 
 from bmeg.vertex import Expression, Aliquot, ExpressionMetric
 from bmeg.edge import ExpressionOf
 from bmeg.emitter import JSONEmitter
+from bmeg.util.cli import default_argument_parser
+from bmeg.util.logging import default_logging
 
 
-emitter = JSONEmitter("tcga")
+def transform(source_path,
+              id_map_file="source/tcga/expression/transcript-level/TCGA_ID_MAP.csv",
+              emitter_directory="tcga"):
 
-# Map CGHub analysis IDs to GDC Aliquot IDs
-id_map_file = "source/tcga/expression/transcript-level/TCGA_ID_MAP.csv"
-r = csv.DictReader(open(id_map_file))
-id_map = {row["CGHubAnalysisID"]: row["Aliquot_id"] for row in r}
+    # check if we are doing one file at time
+    p, file_name = os.path.split(source_path)
+    prefix = file_name.split('_')[1]
+    emitter = JSONEmitter(directory=emitter_directory, prefix=prefix)
+    logging.debug('individual file prefix {}'.format(prefix))
 
-for path in glob("source/tcga/expression/transcript-level/*_tpm.tsv.gz"):
-    reader = csv.reader(gzip.open(path, "rt"), delimiter="\t")
+    # Map CGHub analysis IDs to GDC Aliquot IDs
+    r = csv.DictReader(open(id_map_file))
+    id_map = {row["CGHubAnalysisID"]: row["Aliquot_id"].lower() for row in r}
+
+    reader = csv.reader(gzip.open(source_path, "rt"), delimiter="\t")
     header = next(reader)
     samples = header[1:]
 
@@ -47,4 +59,33 @@ for path in glob("source/tcga/expression/transcript-level/*_tpm.tsv.gz"):
             to_gid=Aliquot.make_gid(aliquot_id)
         )
 
-emitter.close()
+    emitter.close()
+
+
+def make_parallel_workstream(source_path, jobs, dry_run=False):
+    """ equivalent of: ls -1 source/tcga/expression/transcript-level/TCGA_*_tpm.tsv.gz | parallel --jobs 10 python3.7 transform/tcga/expression.py --source_path """
+    with open('/tmp/tcga_expression_transform.txt', 'w') as outfile:
+        for path in glob(source_path):
+            outfile.write("{}\n".format(path))
+    try:
+        cmd = 'cat /tmp/tcga_expression_transform.txt | parallel --jobs {} {} {} --source_path'.format(jobs, sys.executable, __file__)
+        logging.info('running {}'.format(cmd))
+        if not dry_run:
+            subprocess.check_output(cmd, shell=True)
+        else:
+            return cmd
+    except subprocess.CalledProcessError as run_error:
+        logging.exception(run_error)
+        raise ValueError("error code {} {}".format(run_error.returncode, run_error.output))
+
+
+if __name__ == "__main__":
+    parser = default_argument_parser()
+    parser.add_argument("--source_path", default="source/tcga/expression/transcript-level/*_tpm.tsv.gz", help="path to file(s)")
+    parser.add_argument("--jobs", default=10, help="number of jobs to run in parallel")
+    options = parser.parse_args()
+    default_logging(options.loglevel)
+    if '*' in options.source_path:
+        make_parallel_workstream(source_path=options.source_path, jobs=options.jobs)
+    else:
+        transform(source_path=options.source_path)
