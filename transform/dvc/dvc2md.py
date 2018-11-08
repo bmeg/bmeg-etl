bf0a42d7917e55b73e57bcb8adb5c5d74c7e9074
Brian Walsh <brian@bwalsh.com>
2018-11-08 22:41:13 +0000

    adds dvc (data version control)

---
+++ transform/dvc/dvc2md.py
@@ -0,0 +1,72 @@
+import yaml
+import glob
+import types
+import bmeg.ioutils
+from tabulate import tabulate
+
+""" simple transform of dvc into md """
+
+
+DEFAULT_DIRECTORY = 'meta'
+
+
+def transform(
+    source_dir=".",
+    papers_path='source/meta/bmeg-source-data-papers.tsv',
+    emitter_directory=None
+):
+    papers_reader = bmeg.ioutils.read_tsv(papers_path)
+    papers = []
+    for source in papers_reader:
+        papers.append(source)
+
+    if not emitter_directory:
+        emitter_directory = 'outputs/meta'
+
+    with open('{}/dvc.md'.format(emitter_directory), 'w') as f:
+        f.write('# Data Sources Summary\n')
+        f.write(tabulate(papers, headers="keys", tablefmt="pipe"))
+        f.write('\n')
+
+        path = '{}/source*.dvc'.format(source_dir)
+
+        source_data = []
+
+        for filename in glob.iglob(path, recursive=True):
+            with open(filename, 'r') as stream:
+                dvc = yaml.load(stream)
+                if 'cmd' not in dvc:
+                    dvc['cmd'] = ''
+                dvc = types.SimpleNamespace(**dvc)
+                if hasattr(dvc, 'outs'):
+                    for out in dvc.outs:
+                        del out['cache']
+                        out['path'] = out['path'].replace('_', '\\_')
+                        source_data.append(out)
+
+        f.write('# Data Sources Detail\n')
+        f.write(tabulate(source_data, headers="keys", tablefmt="pipe"))
+        f.write('\n')
+
+        path = '{}/output*.dvc'.format(source_dir)
+
+        out_data = []
+
+        for filename in glob.iglob(path, recursive=True):
+            with open(filename, 'r') as stream:
+                dvc = yaml.load(stream)
+                if 'cmd' not in dvc:
+                    dvc['cmd'] = ''
+                dvc = types.SimpleNamespace(**dvc)
+                if hasattr(dvc, 'outs'):
+                    for out in dvc.outs:
+                        out['path'] = out['path'].replace('_', '\\_')
+                    out_data.append({'cmd': dvc.cmd.replace('_', '\\_').split(';')[0], 'output': '<br/>'.join([(out['path']) for out in dvc.outs])})
+
+        f.write('# Transformation Detail\n')
+        f.write(tabulate(out_data, headers="keys", tablefmt="pipe"))
+        f.write('\n')
+
+
+if __name__ == '__main__':
+    transform()
---
+++ transform/dvc/dvc2neo4j.py
@@ -0,0 +1,53 @@
+import yaml
+import glob
+import types
+import os
+
+""" simple transform of dvc into graph. Note: needs access to bolt://..."""
+
+from neo4j import GraphDatabase
+
+driver = GraphDatabase.driver("bolt://10.50.50.229:7687", auth=(os.environ['NEO4J_USERNAME'], os.environ['NEO4J_PASSWORD']))
+
+path = '*.dvc'
+
+with driver.session() as session:
+    for filename in glob.iglob(path, recursive=True):
+        with open(filename, 'r') as stream:
+            dvc = yaml.load(stream)
+            if 'cmd' not in dvc:
+                dvc['cmd'] = ''
+            dvc = types.SimpleNamespace(**dvc)
+            # print(dvc.cmd)
+            # print(dvc.md5)
+            session.run(
+                statement="MERGE (x:Command{md5: {md5} }) SET x = {dict_param}",
+                parameters={'dict_param': {'cmd': dvc.cmd, 'md5': dvc.md5, 'filename': filename}, 'md5': dvc.md5}
+            )
+            if hasattr(dvc, 'deps'):
+                for dep in dvc.deps:
+                    print(filename, dep)
+                    if 'md5' not in dep:
+                        dep['md5'] = 'stub-md5.{}'.format(dep['path'])
+                    session.run(
+                        statement="MERGE (f:File{md5: {md5} }) SET f = {dict_param}",
+                        parameters={'dict_param': dep, 'md5': dep['md5']}
+                    )
+                    session.run(
+                        statement="MATCH (c:Command{md5: {cmd_md5} }) MATCH (f:File{md5: {file_md5} }) CREATE (c)-[:READS]->(f)",
+                        parameters={'cmd_md5': dvc.md5, 'file_md5': dep['md5']}
+                    )
+
+            if hasattr(dvc, 'outs'):
+                for out in dvc.outs:
+                    print(filename, out)
+                    if 'md5' not in out:
+                        out['md5'] = 'stub-md5.{}'.format(out['path'])
+                    session.run(
+                        statement="MERGE (f:File{md5: {md5} }) SET f = {dict_param}",
+                        parameters={'dict_param': out, 'md5': out['md5']}
+                    )
+                    session.run(
+                        statement="MATCH (c:Command{md5: {cmd_md5} }) MATCH (f:File{md5: {file_md5} }) CREATE (c)-[:WRITES]->(f)",
+                        parameters={'cmd_md5': dvc.md5, 'file_md5': out['md5']}
+                    )
