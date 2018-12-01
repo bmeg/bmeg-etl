2d0dee5da1a861411b3120159293145d136efe47
Brian Walsh <brian@bwalsh.com>
2018-12-01 15:36:48 +0000

    refactor to optimze readability

--- transform/dvc/dvc2cytoscape.py
+++ transform/dvc/dvc2cytoscape.py
@@ -16,52 +16,55 @@ def transform(
     emitter_directory=DEFAULT_DIRECTORY,
 ):
     file_keys = ['path', 'md5']
+    graph = []
     dups = []
-    with open('{}/{}/cytoscape.json'.format(output_dir, emitter_directory), 'w') as f:
-        for filename in glob.iglob(dvc_path, recursive=False):
-            with open(filename, 'r') as stream:
-                dvc = yaml.load(stream)
-                if 'cmd' not in dvc:
-                    dvc['cmd'] = '# unknown'
-                dvc['filename'] = filename
-                f.write(json.dumps({'data': {'id': dvc['md5'], 'cmd': dvc['cmd']}}))
-                f.write('\n')
-                if 'deps' in dvc:
-                    for dep in dvc['deps']:
-                        if 'md5' not in dep:
-                            dep['md5'] = 'stub-md5.{}'.format(dep['path'])
-                        for k in [k for k in dep.keys() if k not in file_keys]:
-                            del dep[k]
-                        file = {'data': {'id': dep['md5'], 'path': dep['path']}}
-                        if dep['md5'] not in dups:
-                            f.write(json.dumps(file))
-                            f.write('\n')
-                            dups.append(dep['md5'])
-                        f.write(json.dumps({'data': {
-                                'target': dvc['md5'],
-                                'source': dep['md5'],
-                                'label': 'READS'
-                                }}))
-                        f.write('\n')
-
-                if 'outs' in dvc:
-                    for out in dvc['outs']:
-                        if 'md5' not in out:
-                            out['md5'] = 'stub-md5.{}'.format(out['path'])
-                        for k in [k for k in out.keys() if k not in file_keys]:
-                            del out[k]
-                        file = {'data': {'id': out['md5'], 'path': out['path']}}
-                        if out['md5'] not in dups:
-                            f.write(json.dumps(file))
-                            f.write('\n')
-                            dups.append(out['md5'])
-                        f.write(json.dumps({'data': {
+    for filename in glob.iglob(dvc_path, recursive=False):
+        with open(filename, 'r') as stream:
+            dvc = yaml.load(stream)
+            if 'cmd' not in dvc:
+                dvc['cmd'] = '# unknown'
+            dvc['filename'] = filename
+            graph.append({'data': {'id': dvc['md5'], 'cmd': dvc['cmd'], '_label': 'Command'}})
+            if 'deps' in dvc:
+                for dep in dvc['deps']:
+                    if 'md5' not in dep:
+                        dep['md5'] = 'stub-md5.{}'.format(dep['path'])
+                    for k in [k for k in dep.keys() if k not in file_keys]:
+                        del dep[k]
+                    file = {'data': {'id': dep['md5'], 'path': dep['path'], '_label': 'File'}}
+                    if dep['md5'] not in dups:
+                        graph.append(file)
+                        dups.append(dep['md5'])
+                    graph.append(
+                        {'data': {
+                            'target': dvc['md5'],
+                            'source': dep['md5'],
+                            'path': dep['path'],
+                            'cmd': dvc['cmd'],
+                            '_label': 'READS'
+                        }}
+                    )
+            if 'outs' in dvc:
+                for out in dvc['outs']:
+                    if 'md5' not in out:
+                        out['md5'] = 'stub-md5.{}'.format(out['path'])
+                    for k in [k for k in out.keys() if k not in file_keys]:
+                        del out[k]
+                    file = {'data': {'id': out['md5'], 'path': out['path'], '_label': 'File'}}
+                    if out['md5'] not in dups:
+                        graph.append(file)
+                        dups.append(out['md5'])
+                    graph.append(
+                        {'data': {
                             'target': out['md5'],
                             'source': dvc['md5'],
-                            'edge': 'WRITES'
-                        }
-                        }))
-                        f.write('\n')
+                            'path': out['path'],
+                            'cmd': dvc['cmd'],
+                            '_label': 'WRITES'
+                        }}
+                    )
+    with open('{}/{}/cytoscape.json'.format(output_dir, emitter_directory), 'w') as f:
+        f.write('{}\n'.format(json.dumps(graph)))
 
 
 if __name__ == '__main__':
