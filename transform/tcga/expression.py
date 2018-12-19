e48f7a2ed1080febf91a3879e2315f371bffb53f
Alex Buchanan <buchanae@ohsu.edu>
2018-12-19 13:22:55 -0800

    Emitting gene summeries

--- transform/tcga/expression.py
+++ transform/tcga/expression.py
@@ -16,17 +16,22 @@ from bmeg.util.logging import default_logging
 
 def transform(source_path,
               id_map_file="source/tcga/expression/transcript-level/TCGA_ID_MAP.csv",
+              gene_map_file="source/ensembl/Homo_sapiens.GRCh37.87.chr_patch_hapl_scaff.trans_gene.tsv",
               emitter_directory="tcga"):
 
     # check if we are doing one file at time
     p, file_name = os.path.split(source_path)
     prefix = file_name.split('_')[1]
     emitter = JSONEmitter(directory=emitter_directory, prefix=prefix)
+    emitter_gene = JSONEmitter(directory=emitter_directory, prefix=prefix + "_gene")
     logging.debug('individual file prefix {}'.format(prefix))
 
     # Map CGHub analysis IDs to GDC Aliquot IDs
     r = csv.DictReader(open(id_map_file))
     id_map = {row["CGHubAnalysisID"]: row["Aliquot_id"].lower() for row in r}
+
+    r = csv.reader(open(gene_map_file), delimiter="\t")
+    gene_map = {row[0] : row[1] for row in r}
 
     reader = csv.reader(gzip.open(source_path, "rt"), delimiter="\t")
     header = next(reader)
@@ -37,7 +42,7 @@ def transform(source_path,
 
     for row in reader:
         feature_ids = row[0].split("|")
-        transcript_id = feature_ids[0]
+        transcript_id = feature_ids[0].split(".")[0]
 
         for cghub_id, raw_expr in zip(samples, row[1:]):
             expr = float(raw_expr)
@@ -59,7 +64,30 @@ def transform(source_path,
             to_gid=Aliquot.make_gid(aliquot_id)
         )
 
+        geneValues = {}
+        for k, v in values.items():
+            if k not in gene_map:
+                logging.info("%s=%f not found in mapping" % (k,v))
+            else:
+                gene = gene_map[k]
+                geneValues[gene] = geneValues.get(gene, 0) + v
+
+        gg = Expression(
+            id=aliquot_id,
+            source="tcga",
+            metric=ExpressionMetric.GENE_TPM,
+            method="Illumina Hiseq",
+            values=geneValues,
+        )
+        emitter_gene.emit_vertex(gg)
+        emitter_gene.emit_edge(
+            ExpressionOf(),
+            from_gid=gg.gid(),
+            to_gid=Aliquot.make_gid(aliquot_id)
+        )
+
     emitter.close()
+    emitter_gene.close()
 
 
 def make_parallel_workstream(source_path, jobs, dry_run=False):
