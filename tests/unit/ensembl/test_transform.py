b81e92278bd25f45d5b3b59a0905a924006e40ae
Kyle Ellrott <kellrott@gmail.com>
2018-12-20 23:42:54 -0800

    Fixing assert number

--- tests/unit/ensembl/test_transform.py
+++ tests/unit/ensembl/test_transform.py
@@ -17,5 +17,5 @@ def test_simple(emitter_directory, gff3_path, helpers):
     exon_count = helpers.assert_vertex_file_valid(Exon, '{}/Exon.Vertex.json.gz'.format(emitter_directory))
     gene_count = helpers.assert_vertex_file_valid(Gene, '{}/Gene.Vertex.json.gz'.format(emitter_directory))
     assert transcript_count == 211
-    assert exon_count == 843
+    assert exon_count == 624 # there are 843 entry lines, but 624 unique ids
     assert gene_count == 81
