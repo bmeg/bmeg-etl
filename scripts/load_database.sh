dceadd7473f2770b4eea24845459ad8aab95caa3
adamstruck <adamjstruck@gmail.com>
2019-01-07 22:49:54 +0000

    check all files exist before loading

--- scripts/load_database.sh
+++ scripts/load_database.sh
@@ -2,12 +2,19 @@
 
 if [ "$#" -ne 2 ]; then
 		printf "Illegal number of parameters.\n\n"
-    printf "Usage:\n  load_database.sh <graph> <file_manifest>\n"
+		printf "Usage:\n	load_database.sh <graph> <file_manifest>\n"
 		exit 1
 fi
 
 graph=$1
 file_manifest=$2
+
+for f in $(cat $file_manifest | grep "Vertex"); do
+		if [ ! -f $f ]; then
+				echo "file $f does not exist!"
+				exit 1
+		fi
+done
 
 grip drop $graph
 grip create $graph
@@ -19,7 +26,7 @@ for f in $(cat $file_manifest | grep "Vertex"); do
 				gunzip -c $f | mongoimport -d grip -c ${graph}_vertices --type json $gofast
 		else
 				mongoimport -d grip -c ${graph}_vertices --type json --file $f $gofast
-		fi		
+		fi
 done
 
 for f in $(cat $file_manifest | grep "Edge"); do
@@ -27,5 +34,5 @@ for f in $(cat $file_manifest | grep "Edge"); do
 				gunzip -c $f | mongoimport -d grip -c ${graph}_edges --type json $gofast
 		else
 				mongoimport -d grip -c ${graph}_edges --type json --file $f $gofast
-		fi		
+		fi
 done
