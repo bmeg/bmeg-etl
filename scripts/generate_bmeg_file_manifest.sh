227a1a85720e7c5dcd746290b988f42a8348c6f2
adamstruck <adamjstruck@gmail.com>
2019-01-23 18:01:24 +0000

    added exclusion list to generate_bmeg_file_manifest.sh; regenerated outputs.bmeg_manifest.dvc

--- scripts/generate_bmeg_file_manifest.sh
+++ scripts/generate_bmeg_file_manifest.sh
@@ -13,7 +13,20 @@ VERTEX_FILES=($DIR/outputs/**/*.Vertex.json.gz)
 EDGE_FILES=($DIR/outputs/**/*.Edge.json.gz)
 FILES=(${VERTEX_FILES[@]} ${EDGE_FILES[@]})
 
-EXCEPTIONS=()
+EXCEPTIONS=(
+		"./outputs/ccle/Allele.Vertex.json.gz"
+		"./outputs/g2p/Allele.Vertex.json.gz"
+		"./outputs/g2p/MinimalAllele.Vertex.json.gz"
+		"./outputs/mc3/Allele.Vertex.json.gz"
+		"./outputs/ccle/drug_response.Compound.Vertex.json.gz"
+		"./outputs/g2p/Compound.Vertex.json.gz"
+		"./outputs/gdc/Compound.Vertex.json.gz"
+		"./outputs/gdsc/gdsc.Compound.Vertex.json.gz"
+		"./outputs/g2p/HasEnvironment.Edge.json.gz"
+		"./outputs/ccle/drug_response.ResponseTo.Edge.json.gz"
+		"./outputs/gdsc/gdsc.ResponseTo.Edge.json.gz"
+		"./outputs/gdc/TreatedWith.Edge.json.gz"
+)
 
 echo "generating DVC command..."
 
@@ -31,4 +44,4 @@ DVC_CMD+=" \"python3 transform/dvc/bmeg_file_manifest.py\""
 
 echo "running DVC..."
 
-# echo $DVC_CMD | bash
+echo $DVC_CMD | bash
