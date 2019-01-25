32f0ed6c1228009b6cde6c5b419b20af0941d8db
adamstruck <adamjstruck@gmail.com>
2019-01-25 18:57:31 +0000

    flake8

--- tests/unit/ccle/test_drug_response.py
+++ tests/unit/ccle/test_drug_response.py
@@ -20,12 +20,17 @@ def biosample_path(request):
 
 def validate(helpers, emitter_directory, biosample_path, drug_response_path):
     """ run xform and test results"""
-    profile_file = os.path.join(emitter_directory, 'drug_response.DrugResponse.Vertex.json.gz')
-    profile_in_file = os.path.join(emitter_directory, 'drug_response.ResponseIn.Edge.json.gz')
-    response_to_file = os.path.join(emitter_directory, 'drug_response.ResponseTo.Edge.json.gz')
-    compound_file = os.path.join(emitter_directory, 'drug_response.Compound.Vertex.json.gz')
+    profile_file = os.path.join(
+        emitter_directory, 'drug_response.DrugResponse.Vertex.json.gz')
+    profile_in_file = os.path.join(
+        emitter_directory, 'drug_response.ResponseIn.Edge.json.gz')
+    response_to_file = os.path.join(
+        emitter_directory, 'drug_response.ResponseTo.Edge.json.gz')
+    compound_file = os.path.join(
+        emitter_directory, 'drug_response.Compound.Vertex.json.gz')
 
-    all_files = [profile_file, profile_in_file, response_to_file, compound_file]
+    all_files = [profile_file, profile_in_file,
+                 response_to_file, compound_file]
     # remove output
     with contextlib.suppress(FileNotFoundError):
         for f in all_files:
@@ -42,16 +47,17 @@ def validate(helpers, emitter_directory, biosample_path, drug_response_path):
     helpers.assert_edge_joins_valid(all_files, exclude_labels=['Aliquot'])
     # missing vertexes
     for f in ['drug_response.Aliquot.Vertex.json.gz', 'drug_response.Biosample.Vertex.json.gz',
-             'drug_response.Individual.Vertex.json.gz', 'drug_response.Project.Vertex.json.gz']:
-       v = eval(f.split('.')[1])
-       f = os.path.join(emitter_directory, f)
-       helpers.assert_vertex_file_valid(v, f)
+              'drug_response.Individual.Vertex.json.gz', 'drug_response.Project.Vertex.json.gz']:
+        v = eval(f.split('.')[1])
+        f = os.path.join(emitter_directory, f)
+        helpers.assert_vertex_file_valid(v, f)
     # missing edges
     for f, v1, v2 in [('drug_response.AliquotFor.Edge.json.gz', Aliquot, Biosample),
-                     ('drug_response.BiosampleFor.Edge.json.gz', Biosample, Individual),
-                     ('drug_response.InProject.Edge.json.gz', Individual, Project)]:
-       f = os.path.join(emitter_directory, f)
-       helpers.assert_edge_file_valid(v1, v2, f)
+                      ('drug_response.BiosampleFor.Edge.json.gz',
+                       Biosample, Individual),
+                      ('drug_response.InProject.Edge.json.gz', Individual, Project)]:
+        f = os.path.join(emitter_directory, f)
+        helpers.assert_edge_file_valid(v1, v2, f)
 
 
 def test_simple(helpers, emitter_directory, biosample_path, drug_response_path):
