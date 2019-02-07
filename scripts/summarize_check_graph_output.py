1f8bcf2be1cd6531184f6744b8660fd9266ed0be
adamstruck <adamjstruck@gmail.com>
2019-02-07 21:41:41 +0000

    revert by edge label grouping

--- scripts/summarize_check_graph_output.py
+++ scripts/summarize_check_graph_output.py
@@ -17,7 +17,6 @@ def summarize_output(path, emit_ids):
     input_stream = bmeg.ioutils.reader(path)
     for line in input_stream:
         err = SN(**ujson.loads(line))
-        elabel = err.Label
         if err.msg == "From does not exist":
             label = err.From.split(":")[0]
             gid = err.From
@@ -35,14 +34,10 @@ def summarize_output(path, emit_ids):
             label = "Gene"
 
         if label not in labels:
-            labels[label] = {}
-            labels[label][elabel] = [gid]
+            labels[label] = [gid]
         else:
-            if elabel not in labels[label]:
-                labels[label][elabel] = [gid]
-            else:
-                if gid not in labels[label][elabel]:
-                    labels[label][elabel].append(gid)
+            if gid not in labels[label]:
+                labels[label].append(gid)
 
     if emit_ids:
         print(ujson.dumps(labels))
@@ -50,9 +45,7 @@ def summarize_output(path, emit_ids):
 
     labelCounts = {}
     for k, v in labels.items():
-        labelCounts[k] = {}
-        for ek, ev in v.items():
-            labelCounts[k][ek] = len(ev)
+        labelCounts[k] = len(v)
     print(ujson.dumps(labelCounts))
     return
 
