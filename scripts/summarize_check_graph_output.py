3daa2f5e223cdd03fe92800c33d3deabaccc5901
adamstruck <adamjstruck@gmail.com>
2019-02-07 21:14:54 +0000

    added util to summarize check graph output by missing vertex labels

---
+++ scripts/summarize_check_graph_output.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import argparse
+import os
+import ujson
+from types import SimpleNamespace as SN
+
+import bmeg.ioutils
+
+
+def summarize_output(path, emit_ids):
+    """summarize missing vertices by label"""
+    if not os.path.isfile(path):
+        raise ValueError("path {} is not a file".format(path))
+
+    labels = {}
+    input_stream = bmeg.ioutils.reader(path)
+    for line in input_stream:
+        err = SN(**ujson.loads(line))
+        if err.msg == "From does not exist":
+            label = err.From.split(":")[0]
+            gid = err.From
+        elif err.msg == "To does not exist":
+            label = err.To.split(":")[0]
+            gid = err.To
+        else:
+            raise ValueError("unknown error message: {}".format(err.msg))
+
+        if label.startswith("ENSP"):
+            label = "Protein"
+        elif label.startswith("ENST"):
+            label = "Transcript"
+        elif label.startswith("ENSG"):
+            label = "Gene"
+
+        if label not in labels:
+            labels[label] = [gid]
+        else:
+            if gid not in labels[label]:
+                labels[label].append(gid)
+
+    if emit_ids:
+        print(ujson.dumps(labels))
+        return
+
+    labelCounts = {}
+    for k, v in labels.items():
+        labelCounts[k] = len(v)
+    print(ujson.dumps(labelCounts))
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path",
+                        help="path to check graph output")
+    parser.add_argument("--emit-gids",
+                        default=False,
+                        action="store_true",
+                        help="emit missing gids rather than label counts")
+    args = parser.parse_args()
+    summarize_output(args.path, args.emit_gids)
