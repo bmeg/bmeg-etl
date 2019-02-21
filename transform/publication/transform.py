b6685d18935a8e1d518e2f14f5b616f4af8f5e72
adamstruck <adamjstruck@gmail.com>
2019-02-21 00:31:42 +0000

    reworking stub publication generation

--- transform/publication/transform.py
+++ transform/publication/transform.py
@@ -15,19 +15,47 @@ DEFAULT_DIRECTORY = 'publication'
 
 
 def transform(
-    emitter_name="json",
-    output_dir="outputs",
-    emitter_directory=DEFAULT_DIRECTORY,
-    edge_names="**/*.Edge.json*"
+        emitter_name="json",
+        output_dir="outputs",
+        emitter_directory=DEFAULT_DIRECTORY,
+        vertex_names="**/*Publication.Vertex.json*",
+        edge_names="**/*.HasSupportingReference.Edge.json*"
 ):
     batch_size = 1000
     emitter = new_emitter(name=emitter_name, directory=emitter_directory, prefix='stub')
 
+    # get the existing publications
+    path = '{}/{}'.format(output_dir, vertex_names)
+    files = [filename for filename in glob.iglob(path, recursive=True) if 'stub' not in filename]
+    dedup = []
+    e = v = 0
+    for file in files:
+        logging.info(file)
+        with reader(file) as ins:
+            for line in ins:
+                try:
+                    vertex = ujson.loads(line)
+                    if 'Publication:' != vertex['label']:
+                        logging.debug('Vertex {} does not appear to contain publications. skipping.'.format(file))
+                        break
+                    # get edge components
+                    id = vertex["gid"].replace('Publication:', '').strip()
+                    if id in dedup:
+                        continue
+                    dedup.append(id)
+                    v += 1
+                except Exception as exc:
+                    logging.warning(str(exc))
+                    raise
+                    e += 1
+                if v % batch_size == 0:
+                    logging.info('processed publication vertices: {} errors: {}'.format(v, e))
+    logging.info('processed publication vertices: {} errors: {}'.format(v, e))
+
     # get the edges
     path = '{}/{}'.format(output_dir, edge_names)
     files = [filename for filename in glob.iglob(path, recursive=True) if 'stub' not in filename]
-    c = t = e = 0
-    dedup = []
+    t = e = 0
     for file in files:
         logging.info(file)
         with reader(file) as ins:
@@ -46,15 +74,14 @@ def transform(
                     title = abstract = text = date = author = citation = None
                     publication = Publication(id, title, abstract, text, date, author, citation)
                     emitter.emit_vertex(publication)
-                    c += 1
                     t += 1
                 except Exception as exc:
                     logging.warning(str(exc))
                     raise
                     e += 1
-                if c % batch_size == 0:
+                if t % batch_size == 0:
                     logging.info('transforming read: {} errors: {}'.format(t, e))
-                    c = 0
+
         logging.info('transforming read: {} errors: {}'.format(t, e))
         emitter.close()
 
@@ -63,5 +90,4 @@ if __name__ == '__main__':  # pragma: no cover
     parser = default_argument_parser()
     options = parser.parse_args(sys.argv[1:])
     default_logging(options.loglevel)
-
     transform()
