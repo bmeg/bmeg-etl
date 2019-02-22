893ddc9116047b24ff8f4a22eddf4dc7c559b487
adamstruck <adamjstruck@gmail.com>
2019-02-22 19:48:29 +0000

    ran new stub publication generator

--- transform/publication/transform.py
+++ transform/publication/transform.py
@@ -19,71 +19,67 @@ def transform(
         output_dir="outputs",
         emitter_directory=DEFAULT_DIRECTORY,
         vertex_names="**/*Publication.Vertex.json*",
-        edge_names="**/*.HasSupportingReference.Edge.json*"
+        edge_names="**/*HasSupportingReference.Edge.json*"
 ):
-    batch_size = 1000
-    emitter = new_emitter(name=emitter_name, directory=emitter_directory, prefix='stub')
-
-    # get the existing publications
+    # get the existing publication vertices
     path = '{}/{}'.format(output_dir, vertex_names)
     files = [filename for filename in glob.iglob(path, recursive=True) if 'stub' not in filename]
-    dedup = []
-    e = v = 0
+    nfiles = len(files)
+    dedup = {}
+    f = v = 0
     for file in files:
-        logging.info(file)
+        f += 1
+        logging.info("processing publication file: {}/{}".format(f, nfiles))
         with reader(file) as ins:
             for line in ins:
                 try:
                     vertex = ujson.loads(line)
-                    if 'Publication:' != vertex['label']:
-                        logging.debug('Vertex {} does not appear to contain publications. skipping.'.format(file))
+                    if 'Publication' != vertex['label']:
+                        logging.info('Vertex {} does not appear to contain publications. skipping.'.format(file))
                         break
-                    # get edge components
-                    id = vertex["gid"].replace('Publication:', '').strip()
-                    if id in dedup:
-                        continue
-                    dedup.append(id)
+                    dedup[vertex["gid"]] = True
                     v += 1
                 except Exception as exc:
-                    logging.warning(str(exc))
-                    raise
-                    e += 1
-                if v % batch_size == 0:
-                    logging.info('processed publication vertices: {} errors: {}'.format(v, e))
-    logging.info('processed publication vertices: {} errors: {}'.format(v, e))
+                    logging.error(str(exc))
+                    raise exc
+    logging.info('processed publication vertices: {}'.format(v))
 
-    # get the edges
+    # get the edges connected to publications
+    batch_size = 1000
+    emitter = new_emitter(name=emitter_name, directory=emitter_directory, prefix='stub')
     path = '{}/{}'.format(output_dir, edge_names)
     files = [filename for filename in glob.iglob(path, recursive=True) if 'stub' not in filename]
-    t = e = 0
+    nfiles = len(files)
+    e = f = r = 0
     for file in files:
-        logging.info(file)
+        f += 1
+        logging.info("processing HasSupportingReference file: {}/{}".format(f, nfiles))
         with reader(file) as ins:
             for line in ins:
                 try:
                     edge = ujson.loads(line)
                     if 'Publication:' not in edge['gid']:
-                        logging.debug('Edge {} has no publications that need transformation. skipping.'.format(file))
+                        logging.info('Edge {} has no publications that need transformation. skipping.'.format(file))
                         break
                     # get edge components
                     to = edge['to']
-                    id = to.replace('Publication:', '').strip()
-                    if id in dedup:
+                    if to in dedup:
+                        r += 1
                         continue
-                    dedup.append(id)
+                    dedup[to] = True
+                    id = to.replace('Publication:', '')
                     title = abstract = text = date = author = citation = None
                     publication = Publication(id, title, abstract, text, date, author, citation)
                     emitter.emit_vertex(publication)
-                    t += 1
-                except Exception as exc:
-                    logging.warning(str(exc))
-                    raise
                     e += 1
-                if t % batch_size == 0:
-                    logging.info('transforming read: {} errors: {}'.format(t, e))
-
-        logging.info('transforming read: {} errors: {}'.format(t, e))
-        emitter.close()
+                except Exception as exc:
+                    logging.error(str(exc))
+                    raise exc
+                if e % batch_size == 0:
+                    logging.info('emitted stub publication vertices: {}'.format(e))
+    logging.info('emitted stub publication vertices: {}'.format(e))
+    logging.info('existing publication refs found: {}'.format(r))
+    emitter.close()
 
 
 if __name__ == '__main__':  # pragma: no cover
