95690eb1cda9bbcf7c5d5dc635300596fe1f56ef
adamstruck <adamjstruck@gmail.com>
2019-04-29 23:34:57 +0000

    gdsc unit tests

--- transform/gdsc/drug_response.py
+++ transform/gdsc/drug_response.py
@@ -8,7 +8,6 @@ from bmeg.enrichers.drug_enricher import compound_factory
 
 
 def transform(project_lookup_path="source/ccle/cellline_project_lookup.tsv",
-              cellline_meta_path="source/gdsc/Cell_Lines_Details.xlsx",
               drugs_meta_path="source/gdsc/Screened_Compounds.xlsx",
               ic50_path="source/gdsc/GDSC_IC50.csv",
               auc_path="source/gdsc/GDSC_AUC.csv",
@@ -20,14 +19,15 @@ def transform(project_lookup_path="source/ccle/cellline_project_lookup.tsv",
     emitter = JSONEmitter(prefix=emitter_prefix, directory=emitter_directory)
 
     drugs_df = pandas.read_excel(drugs_meta_path, sheet_name=0, header=0)
-    drugs_df.set_index("DRUG_ID")
+    drugs_df.set_index("DRUG_ID", inplace=True, drop=True)
 
     auc_df = pandas.read_csv(auc_path, sep=",", index_col=0)
-    auc_df.index = [x.replace('GDSC:', '') for x in auc_df.index.tolist()]
+    auc_df.index = [int(x.replace('GDSC:', '')) for x in auc_df.index.tolist()]
 
     ic50_df = pandas.read_csv(ic50_path, sep=",", index_col=0)
-    ic50_df.index = [x.replace('GDSC:', '') for x in ic50_df.index.tolist()]
+    ic50_df.index = [int(x.replace('GDSC:', '')) for x in ic50_df.index.tolist()]
 
+    emitted_compounds = {}
     project_compounds = {}
     for drug_id, row in ic50_df.iterrows():
         drug_name = drugs_df.loc[drug_id]['DRUG_NAME']
@@ -35,7 +35,7 @@ def transform(project_lookup_path="source/ccle/cellline_project_lookup.tsv",
             auc_val = auc_df.loc[drug_id][cellline_id]
 
             # Track drugs for project
-            project_id = "GDSC_{}" % (projects.get(cellline_id), "Unknown")
+            project_id = "GDSC_%s" % (projects.get(cellline_id, "Unknown"))
             proj = Project(project_id)
             if proj.gid() not in project_compounds:
                 project_compounds[proj.gid()] = {}
@@ -47,6 +47,9 @@ def transform(project_lookup_path="source/ccle/cellline_project_lookup.tsv",
                               ic50=ic50_val)
             emitter.emit_vertex(dr)
             compound = compound_factory(name=drug_name)
+            if compound.gid() not in emitted_compounds:
+                emitter.emit_vertex(compound)
+                emitted_compounds[compound.gid()] = True
             emitter.emit_edge(
                 ResponseIn(),
                 dr.gid(),
