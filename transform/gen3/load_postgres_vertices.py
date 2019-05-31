54ff2929feb75737b691ca31540e8f1f172abf2c
Brian Walsh <brian@bwalsh.com>
2019-05-31 16:47:49 +0000

    sets schema category

--- transform/gen3/load_postgres_vertices.py
+++ transform/gen3/load_postgres_vertices.py
@@ -1,12 +1,13 @@
 import sys
-import inflection
 
 from transform.gen3.gen3_util import get_class_tablename_from_id
 from transform.gen3.cli import default_args
+from transform.gen3.schemas import make_id
 
 
 def transform(project_id, vertex_name, output_stream=sys.stdout):
-    table_name = get_class_tablename_from_id(inflection.underscore(vertex_name))
+    """Writes transform and load commands to output_stream."""
+    table_name = get_class_tablename_from_id(make_id(vertex_name))
     output_stream.write("python transform/gen3/postgres_vertices.py --project_id {} --vertex_name {} | $psql -c \"copy {}(node_id, acl, _sysan,  _props) from stdin  csv delimiter E'\\x01' quote E'\\x02' ;\"".format(project_id, vertex_name, table_name))
 
 
