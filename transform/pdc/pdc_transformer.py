10150c4e91ecc33b81a338906a1dbaae241c7ef1
Brian Walsh <brian@bwalsh.com>
2019-08-11 21:41:46 +0000

    misc

--- transform/pdc/pdc_transformer.py
+++ transform/pdc/pdc_transformer.py
@@ -43,7 +43,6 @@ class PDCTransformer():
         G.add_edges_from([('pdc',k) for k in set([case.project_submitter_id for case in self.all_cases])], label='projects')
         
         G.add_nodes_from(set([case.case_submitter_id for case in self.all_cases]), label='Case')
-
         G.add_edges_from([(case.project_submitter_id, case.case_submitter_id) for case in self.all_cases], label='cases')
 
         def case_samples():
