f2b75ffc9d295f9806bd0b5d0827d55d1170272b
adamstruck <adamjstruck@gmail.com>
2019-08-15 22:20:08 +0000

    pathway commons - remove PathwayComponent

--- transform/pathway_commons/transform.py
+++ transform/pathway_commons/transform.py
@@ -3,11 +3,9 @@ import logging
 import sys
 
 import bmeg.enrichers.gene_enricher as gene_enricher
-from bmeg import (Pathway, PathwayComponent, Interaction, Gene, Publication, Project,
-                  Pathway_PathwayComponents_PathwayComponent, Pathway_Interactions_Interaction, Pathway_Genes_Gene, Pathway_SubPathways_Pathway,
-                  PathwayComponent_Interactions_Interaction, PathwayComponent_Genes_Gene,
-                  Interaction_Genes_Gene, Interaction_Publications_Publication,
-                  Gene_PathwayComponents_PathwayComponent, Gene_Interactions_Interaction)
+from bmeg import (Pathway, Interaction, Gene, Publication, Project,
+                  Pathway_Interactions_Interaction, Pathway_Genes_Gene, Pathway_SubPathways_Pathway,
+                  Interaction_Genes_Gene, Gene_Interactions_Interaction, Interaction_Publications_Publication)
 from bmeg.emitter import JSONEmitter
 from bmeg.ioutils import read_tsv
 
@@ -21,7 +19,7 @@ def get_participant(p):
         gene = gene_enricher.get_gene(p)
         ens_id = gene.get("ensembl_gene_id", None)
         if ens_id is None:
-            raise ValueError("No ensembl id found for entrez_id: {}".format(p))
+            raise ValueError("No ensembl id found for: {}".format(p))
         v = Gene(id=Gene.make_gid(ens_id))
     except Exception as e:
         logging.error(e)
@@ -29,7 +27,7 @@ def get_participant(p):
 
 
 def transform(sif_file="source/pathway_commons/pc11.detailed.sif",
-              pathways_file="source/pathway_commons/pathways.txt",
+              pathways_file="source/pathway_commons/pathways.txt.gz",
               emitter_prefix=None,
               emitter_directory="pathway_commons"):
 
@@ -41,10 +39,7 @@ def transform(sif_file="source/pathway_commons/pc11.detailed.sif",
                                "PATHWAY_URI", "RESOURCE", "SOURCE_LOC", "TARGET_LOC"],
                    quoting=csv.QUOTE_NONE)
 
-    components = {}
-    path_comp = {}
     path_int = {}
-    gene_comp = {}
     gene_path = {}
     for line in sif:
         # ignore compounds production for now
@@ -90,84 +85,40 @@ def transform(sif_file="source/pathway_commons/pc11.detailed.sif",
                     emit_backref=True
                 )
 
-        # emit pathway components and edges
-        for m in line["MEDIATOR"].split(";"):
-            comp = PathwayComponent(
-                id=PathwayComponent.make_gid(m),
-                project_id=Project.make_gid("Reference"),
-            )
-            if m not in components:
-                emitter.emit_vertex(comp)
-                components[m] = True
-            emitter.emit_edge(
-                PathwayComponent_Interactions_Interaction(
-                    from_gid=comp.gid(),
-                    to_gid=i.gid()
-                ),
-                emit_backref=True
-            )
-            if (pa.gid(), comp.gid()) not in gene_comp:
-                emitter.emit_edge(
-                    Gene_PathwayComponents_PathwayComponent(
-                        from_gid=pa.gid(),
-                        to_gid=comp.gid()
-                    ),
-                    emit_backref=False
+        # emit pathway edges
+        for p in line["PATHWAY_URI"].split(";"):
+            if p != "":
+                pathway = Pathway(
+                    id=Pathway.make_gid(p),
                 )
-                gene_comp[(pa.gid(), comp.gid())] = True
-            if (comp.gid(), pb.gid()) not in gene_comp:
-                emitter.emit_edge(
-                    PathwayComponent_Genes_Gene(
-                        from_gid=comp.gid(),
-                        to_gid=pb.gid()
-                    ),
-                    emit_backref=False
-                )
-                gene_comp[(comp.gid(), pb.gid())] = True
-
-            # emit pathway edges
-            for p in line["PATHWAY_URI"].split(";"):
-                if p != "":
-                    pathway = Pathway(
-                        id=Pathway.make_gid(p),
+                if i.gid() not in path_int:
+                    emitter.emit_edge(
+                        Pathway_Interactions_Interaction(
+                            from_gid=pathway.gid(),
+                            to_gid=i.gid(),
+                        ),
+                        emit_backref=True
                     )
-                    if comp.gid() not in path_comp:
-                        emitter.emit_edge(
-                            Pathway_PathwayComponents_PathwayComponent(
-                                from_gid=pathway.gid(),
-                                to_gid=comp.gid(),
-                            ),
-                            emit_backref=True
-                        )
-                        path_comp[comp.gid()] = True
-                    if i.gid() not in path_int:
-                        emitter.emit_edge(
-                            Pathway_Interactions_Interaction(
-                                from_gid=pathway.gid(),
-                                to_gid=i.gid(),
-                            ),
-                            emit_backref=True
-                        )
-                        path_int[i.gid()] = True
-                    # Pathway -> Gene edges are bidirectional
-                    if pa.gid() not in gene_path:
-                        emitter.emit_edge(
-                            Pathway_Genes_Gene(
-                                from_gid=pathway.gid(),
-                                to_gid=pa.gid()
-                            ),
-                            emit_backref=True
-                        )
-                        gene_path[pa.gid()] = True
-                    if pb.gid() not in gene_path:
-                        emitter.emit_edge(
-                            Pathway_Genes_Gene(
-                                from_gid=pathway.gid(),
-                                to_gid=pb.gid()
-                            ),
-                            emit_backref=True
-                        )
-                        gene_path[pb.gid()] = True
+                    path_int[i.gid()] = True
+                # Pathway -> Gene edges are bidirectional
+                if pa.gid() not in gene_path:
+                    emitter.emit_edge(
+                        Pathway_Genes_Gene(
+                            from_gid=pathway.gid(),
+                            to_gid=pa.gid()
+                        ),
+                        emit_backref=True
+                    )
+                    gene_path[pa.gid()] = True
+                if pb.gid() not in gene_path:
+                    emitter.emit_edge(
+                        Pathway_Genes_Gene(
+                            from_gid=pathway.gid(),
+                            to_gid=pb.gid()
+                        ),
+                        emit_backref=True
+                    )
+                    gene_path[pb.gid()] = True
 
     # emit pathway hierarchy
     # PATHWAY_URI	DISPLAY_NAME	DIRECT_SUB_PATHWAY_URIS	ALL_SUB_PATHWAY_URIS
