d31be97570bfeb9d2ac92552f87b887ae4d54d81
adamstruck <adamjstruck@gmail.com>
2019-11-01 20:15:28 +0000

    added pharmacodb drug response transform

--- transform/pharmacodb/drug_response.py
+++ transform/pharmacodb/drug_response.py
@@ -8,30 +8,19 @@ from bmeg import (Aliquot, DrugResponse, Project,
 from bmeg.enrichers.drug_enricher import compound_factory
 
 
-def transform(cellline_lookup_path='source/ccle/cellline_lookup.tsv',
+def transform(cellline_lookup_path='source/ccle/cellline_id_lookup.tsv',
               cells_path='source/pharmacodb/cells.csv',
               drugs_path='source/pharmacodb/drugs.csv',
               drug_annots_path='source/pharmacodb/drug_annots.csv',
               experiments_path='source/pharmacodb/experiments.csv',
               dose_response_path='source/pharmacodb/dose_responses.csv',
               profiles_path='source/pharmacodb/profiles.csv',
-              source_drug_path='source/pharmacodb/source_drug_names.csv',
               emitter_prefix=None,
               emitter_directory='pharmacodb'):
 
-    cellline_lookup_path='source/ccle/cellline_lookup.tsv'
-
-    cells_path='source/pharmacodb/cells.csv'
-    drugs_path='source/pharmacodb/drugs.csv'
-    drug_annots_path='source/pharmacodb/drug_annots.csv'
-    experiments_path='source/pharmacodb/experiments.csv'
-    dose_response_path='source/pharmacodb/dose_responses.csv'
-    profiles_path='source/pharmacodb/profiles.csv'
-    source_drug_path='source/pharmacodb/source_drug_names.csv'
-
-    celllines = bmeg.ioutils.read_lookup(cellline_lookup_path)
-
     emitter = JSONEmitter(directory=emitter_directory, prefix=emitter_prefix)
+
+    cellline_id_lookup = bmeg.ioutils.read_lookup(cellline_lookup_path)
 
     dose_response = pandas.read_csv(dose_response_path)
 
@@ -40,7 +29,6 @@ def transform(cellline_lookup_path='source/ccle/cellline_lookup.tsv',
     drug_annots = pandas.read_csv(drug_annots_path)
     experiments = pandas.read_csv(experiments_path)
     profiles = pandas.read_csv(profiles_path)
-    source_drug_names = pandas.read_csv(source_drug_path)
 
     merged_data = pandas.merge(
         cells[['cell_id', 'cell_name']],
@@ -49,12 +37,8 @@ def transform(cellline_lookup_path='source/ccle/cellline_lookup.tsv',
             pandas.merge(
                 experiments,
                 pandas.merge(
-                    source_drug_names.rename(columns={'drug_name': 'source_drug_name'}, inplace=False)[['drug_id', 'source_drug_name']],
-                    pandas.merge(
-                        drugs[['drug_id', 'drug_name']],
-                        drug_annots,
-                        on='drug_id'
-                    ),
+                    drugs[['drug_id', 'drug_name']],
+                    drug_annots,
                     on='drug_id'
                 ),
                 on='drug_id'
@@ -71,8 +55,59 @@ def transform(cellline_lookup_path='source/ccle/cellline_lookup.tsv',
     merged_data = merged_data[merged_data.dataset_id.isin([1, 2, 5])]
     merged_data['dataset_name'] = merged_data.dataset_id.map({1: 'CCLE', 2: 'CTRP', 5: 'GDSC'})
 
+    emitted_compounds = {}
     for index, row in merged_data.iterrows():
-        cell_name = celllines.get(row.cell_name, row.cell_name)
+        cell_name = cellline_id_lookup.get(row.cell_name, row.cell_name)
+        dr = DrugResponse(
+            id=DrugResponse.make_gid(row.dataset_name, cell_name, row.drug_name),
+            einf=row.EINF,
+            ec50=row.EC50,
+            ic50=row.IC50,
+            aac=row.AAC,
+            hs=row.HS,
+            dss1=row.DSS1,
+            dss2=row.DSS2,
+            dss3=row.DSS3,
+            dose_um=dose_response[dose_response.experiment_id == row.experiment_id].dose.tolist(),
+            response=dose_response[dose_response.experiment_id == row.experiment_id].response.tolist(),
+            source_cell_name=row.cell_name,
+            source_drug_name=row.drug_name,
+            project_id=Project.make_gid(row.dataset_name)
+        )
+        emitter.emit_vertex(dr)
+
+        drug_name = row.drug_name
+        if not pandas.isna(row.pubchem):
+            drug_name = "CID%s" % row.pubchem
+        compound = compound_factory(name=drug_name)
+
+        if compound.gid() not in emitted_compounds:
+            emitter.emit_vertex(compound)
+            emitter.emit_edge(
+                Compound_Projects_Project(
+                    from_gid=compound.gid(),
+                    to_gid=Project.make_gid(row.dataset_name)
+                ),
+                emit_backref=True
+            )
+            emitted_compounds[compound.gid()] = None
+
+        # add edge to compound
+        emitter.emit_edge(
+            DrugResponse_Compounds_Compound(
+                from_gid=dr.gid(),
+                to_gid=compound.gid()
+            ),
+            emite_backred=True,
+        )
+        # add edge to aliquot
+        emitter.emit_edge(
+            DrugResponse_Aliquot_Aliquot(
+                from_gid=dr.gid(),
+                to_gid=Aliquot.make_gid("%s:%s" % (row.dataset_name, cell_name))
+            ),
+            emit_backref=True
+        )
 
     emitter.close()
 
