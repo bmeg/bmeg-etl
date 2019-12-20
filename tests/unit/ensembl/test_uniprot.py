569408b12249ec8605c53fb717674b91c1e74f49
adamstruck <adamjstruck@gmail.com>
2019-12-20 18:21:38 +0000

    update ensembl transform unit tests

--- tests/unit/ensembl/test_uniprot.py
+++ tests/unit/ensembl/test_uniprot.py
@@ -3,7 +3,7 @@ import os
 import contextlib
 import shutil
 
-from transform.ensembl.proteins import transform
+from transform.ensembl.uniprot import transform
 
 
 @pytest.fixture
@@ -13,9 +13,9 @@ def protein_table_path(request):
 
 
 def test_simple(helpers, emitter_directory, protein_table_path):
-    protein_file = os.path.join(emitter_directory, 'Protein.Vertex.json.gz')
-    tpp_edge_file = os.path.join(emitter_directory, 'Transcript_Protein_Protein.Edge.json.gz')
-    ptt_edge_file = os.path.join(emitter_directory, 'Protein_Transcript_Transcript.Edge.json.gz')
+    uniprot_file = os.path.join(emitter_directory, 'Uniprot.Vertex.json.gz')
+    tpp_edge_file = os.path.join(emitter_directory, 'Protein_Uniprot_Uniprot.Edge.json.gz')
+    ptt_edge_file = os.path.join(emitter_directory, 'Uniprot_Protein_Protein.Edge.json.gz')
 
     # remove output
     with contextlib.suppress(FileNotFoundError):
@@ -23,8 +23,8 @@ def test_simple(helpers, emitter_directory, protein_table_path):
 
     transform(protein_table_path=protein_table_path, emitter_directory=emitter_directory)
 
-    helpers.assert_vertex_file_valid(protein_file)
+    helpers.assert_vertex_file_valid(uniprot_file)
     helpers.assert_edge_file_valid(tpp_edge_file)
     helpers.assert_edge_file_valid(ptt_edge_file)
-    helpers.assert_edge_joins_valid([protein_file, tpp_edge_file, ptt_edge_file],
-                                    exclude_labels=['Transcript'])
+    helpers.assert_edge_joins_valid([uniprot_file, tpp_edge_file, ptt_edge_file],
+                                    exclude_labels=['Protein'])
