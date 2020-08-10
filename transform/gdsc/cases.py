d20bdc14bceac5cbb10ebc594263601ce00e2a51
Kyle Ellrott <kellrott@gmail.com>
2020-08-10 08:06:59 -0700

    Updating gdsc cases transform

--- transform/gdsc/cases.py
+++ transform/gdsc/cases.py
@@ -18,7 +18,7 @@ from bmeg.emitter import JSONEmitter
 def transform(cellline_lookup_path="source/ccle/cellline_id_lookup.tsv",
               properties_lookup_path="source/ccle/cellline_properties_lookup.tsv",
               phenotype_lookup_path="source/ccle/cellline_phenotype_lookup.tsv",
-              pharmacodb_cells_path="source/pharmacodb/cells.csv",
+              pharmacodb_cells_path="source/pharmacodb/cells.tsv.gz",
               pharmacodb_experiments_path="source/pharmacodb/experiments.csv",
               emitter_prefix='gdsc',
               emitter_directory='gdsc'):
@@ -46,7 +46,7 @@ def transform(cellline_lookup_path="source/ccle/cellline_id_lookup.tsv",
 
     pharmacodb = pandas.merge(
         pandas.read_csv(pharmacodb_experiments_path)[["cell_id", "dataset_id"]],
-        pandas.read_csv(pharmacodb_cells_path)[["cell_id", "cell_name"]],
+        pandas.read_csv(pharmacodb_cells_path, sep="\t")[["cell_id", "cell_name"]],
         on="cell_id"
     ).drop_duplicates()
     pharmacodb_gdsc = pharmacodb[pharmacodb.dataset_id == 5]
