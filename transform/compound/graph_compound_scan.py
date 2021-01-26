2cf940ec0337a4a617b4b162d62e35dce8624ecc
Kyle Ellrott <kellrott@gmail.com>
2021-01-25 17:01:11 -0800

    Adding in missing command

---
+++ transform/compound/graph_compound_scan.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import gzip
+
+o = list()
+
+for root, dirs, files in os.walk(sys.argv[1]):
+    for name in files:
+        if "Compound" in name:
+            f = os.path.join(root, name)
+            with gzip.open(f) as handle:
+                for line in handle:
+                    d = json.loads(line)
+                    x = d.get("gid", "")
+                    if x.startswith("Compound"):
+                        o.append(x)
+                    x = d.get("to", "")
+                    if x.startswith("Compound"):
+                        o.append(x)
+                    x = d.get("from", "")
+                    if x.startswith("Compound"):
+                        o.append(x)
+for i in set(o):
+    print(i.replace("Compound:", ""))
