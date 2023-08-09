e5c99f1a6202c111aad3e91734d613bff1e701b3
Zachary Everton <evertonzachary@gmail.com>
2023-08-09 14:30:04 -0700

    Removed commented-out code

--- normalize/allele/effectMerge.py
+++ normalize/allele/effectMerge.py
@@ -6,7 +6,6 @@ import polars as pl
 import argparse
 import json
 
-k = 100
 parser = argparse.ArgumentParser(description="Basically annotates a vcf of allele effects using the amino acid equivalent of a bed file")
 parser.add_argument("-f", "--featuresFile", help="json.gz of features to match up (has transcript, start and end cols)")
 parser.add_argument("-v", "--variantsFile", help="json.gz of variants to match up (has ensembl_transcript and aa_position cols")
@@ -42,22 +41,6 @@ preFinal = iTest.groupby(['aa_position', 'transcript']).agg(pl.all().drop_nulls(
 
 final = variants.join(preFinal, left_on=['aa_position', 'ensembl_transcript'],right_on=['aa_position','transcript'], how='left')
 
-
-
-
-#while len(var1) > 0:
-#  merge1 = var1[:k].join(features, left_on='ensembl_transcript', right_on='transcript', how='left')
-#  var1 = var1[k:]
-#  merge2 = merge1.filter(pl.col('aa_position').is_between(pl.col('start'),pl.col('end'))).sort('id').select(pl.all().exclude('start','end'))
-#  
-#  merge3 = merge2.groupby('id').agg(pl.all().drop_nulls().unique().first())
-#  #merge2.groupby('id').agg(pl.all().drop_nulls().explode().unique().cast(pl.Utf8).str.concat(', '))
-#  
-#  for row in merge3.iter_rows(named=True):
-#    rowd = {k:v for k,v in row.items() if v is not None}
-#    #if "sampleid" in rowd and "treatmentid" in rowd:
-#    print(json.dumps(rowd))
-#
 for row in final.iter_rows(named=True):
   rowd = {k:v for k,v in row.items() if v is not None}
   #if "sampleid" in rowd and "treatmentid" in rowd:
