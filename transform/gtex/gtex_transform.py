c77a33b088aa8306709cc48eeb07952b0863589d
teslajoy <nasim@plenary.org>
2024-04-29 15:28:34 -0700

    removed commented code

--- transform/gtex/gtex_transform.py
+++ transform/gtex/gtex_transform.py
@@ -1,16 +1,6 @@
 
 
 def build_patient(row):
-    """
-    import orjson
-    from fhir.resources.patient import Patient
-    from fhir.resources.identifier import Identifier
-
-    patient_identifier = Identifier(**{"value": row['SUBJID'], "system": "".join(["https://gtexportal.org/", "subject"])})
-    patient = Patient(**{"id": "-".join([row['SUBJID'], "gtex"]), "identifier": [patient_identifier], "birthDate": row['fhir_birthDate'], "gender": row['SEX_string']})
-    return orjson.loads(patient.json())
-    """
-
     # TODO: replace and confirm BMEG id def
     patient = {
         "resourceType": "Patient",
@@ -29,17 +19,6 @@ def build_patient(row):
 
 
 def build_specimen(row):
-    """
-    import orjson
-    from fhir.resources.specimen import Specimen
-    from fhir.resources.reference import Reference
-    from fhir.resources.codeableconcept import CodeableConcept
-
-    cc = CodeableConcept(**{"coding": [{"system": "http://snomed.info/sct", "display": row['SMTSD'], "code": str(row['sctid'])}]})
-    subject_ref = Reference(**{"reference": "/".join(["Patient", "-".join([row['SUBJID'], "gtex"])])})
-    specimen = Specimen(**{"id": "-".join([row['SAMPID'], "gtex"]), "type": cc, "subject": subject_ref})
-    return orjson.loads(specimen.json())
-    """
     # TODO: replace and confirm BMEG id def
     sample = {
         "resourceType": "Specimen",
