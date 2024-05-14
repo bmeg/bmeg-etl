71cda3679e8de25856802248ee58c365c2369720
teslajoy <nasim@plenary.org>
2024-05-14 07:42:45 -0700

    updated plan - fetch dataset & experiments

---
+++ transform/pharmacodb/fetch_experiments.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+fetch_experiments() {
+    local page=$1
+    local per_page=$2
+    local output_file=$3
+    curl -s -X POST https://pharmacodb.ca/graphql \
+        -H "Content-Type: application/json" \
+        -d '{"query":"{ experiments(page: '"$page"', per_page: '"$per_page"') { id cell_line { id name tissue { id name } } compound { id name annotation { smiles inchikey pubchem chembl fda_status } } tissue { id name } dataset { id name } profile { HS Einf EC50 AAC IC50 DSS1 DSS2 DSS3 } dose_response { dose response } } }"}' \
+        -o "$output_file"
+}
+
+fetch_all_experiments() {
+    local per_page=50
+    local page=1
+    local output_file="experiments.json"
+    while true; do
+        fetch_experiments $page $per_page $output_file
+
+        if ! grep -q '"experiments":\[\]' "$output_file"; then
+            echo "fetched page:  $page"
+            ((page++))
+        else
+            echo "all done!"
+            break
+        fi
+    done
+}
+
+fetch_all_experiments
