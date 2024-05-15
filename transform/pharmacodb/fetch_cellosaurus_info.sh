b4e17cc1a5fcdfd9be13a7795899e4b559f5a60c
teslajoy <nasim@plenary.org>
2024-05-15 09:49:15 -0700

    updating transform

--- transform/pharmacodb/fetch_cellosaurus_info.sh
+++ transform/pharmacodb/fetch_cellosaurus_info.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-
 fetch_cell_info() {
-	# name is sample_id 
-	# accession_id is cellosaurus id
-	# diseases is cellosaurus disease
-	local output_file="cell_lines_cellosaurus_mapping.json"
-	    curl -s -X POST https://pharmacodb.ca/graphql \
+    local celline_id=$1
+    echo "in fetch_cell_info - fetching info for cell-line ID: $celline_id"
+    curl -s -X POST https://pharmacodb.ca/graphql \
         -H "Content-Type: application/json" \
-        -d '{"query":"{ cell_line(: '"$cellId"') { id name accession_id diseases tissue { id name } synonyms { name  dataset { id name } } } "}' \
-        -o "$output_file"
+        -d '{"query":"{ cell_line(cellId: '"$celline_id"') { id name accession_id diseases tissue { id name } synonyms { name dataset { id name } } } }"}' | \
+        jq -c '.data.cell_line' >> "../../source/pharmacodb/graphql_dump/cells_cellosaurus_info.ndjson"
 }
+
+fetch_all_cell_line_info() {
+    > "../../source/pharmacodb/graphql_dump/cells_cellosaurus_info.ndjson"
+
+    jq -r '.data.cell_lines[].id' ../../source/pharmacodb/graphql_dump/cell_lines.json | while read celline_id; do
+        echo "iterating over cell-line ID: $celline_id"
+        fetch_cell_info $celline_id
+    done
+}
+
+fetch_all_cell_line_info
